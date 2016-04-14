055282ceb68541c0d0341050357a37cc357497ba
exch-bms2 <exch-bms2@users.noreply.github.com>
2016-04-15 03:33:11 +0900

    Music Select : Refactoring

---
+++ src/bms/player/beatoraja/select/Bar.java
@@ -0,0 +1,161 @@
+package bms.player.beatoraja.select;
+
+import bms.player.lunaticrave2.FolderData;
+import bms.player.lunaticrave2.IRScoreData;
+import bms.player.lunaticrave2.SongData;
+
+public abstract class Bar {
+
+	private IRScoreData score;
+
+	public abstract String getTitle();
+
+	public IRScoreData getScore() {
+		return score;
+	}
+
+	public void setScore(IRScoreData score) {
+		this.score = score;
+	}
+}
+
+class SongBar extends Bar {
+
+	private SongData song;
+
+	/**
+	 * リプレイデータが存在するか
+	 */
+	private boolean existsReplay;
+
+	public SongBar(SongData song) {
+		this.song = song;
+	}
+
+	public SongData getSongData() {
+		return song;
+	}
+
+	@Override
+	public String getTitle() {
+		return song.getTitle();
+	}
+
+	public boolean existsReplayData() {
+		return existsReplay;
+	}
+
+	public void setExistsReplayData(boolean existsReplay) {
+		this.existsReplay = existsReplay;
+	}
+}
+
+class FolderBar extends Bar {
+
+	private FolderData folder;
+	private String crc;
+
+	public FolderBar(FolderData folder, String crc) {
+		this.folder = folder;
+		this.crc = crc;
+	}
+
+	public FolderData getFolderData() {
+		return folder;
+	}
+
+	public String getCRC() {
+		return crc;
+	}
+
+	@Override
+	public String getTitle() {
+		return folder.getTitle();
+	}
+}
+
+class TableBar extends Bar {
+
+	private String name;
+	private TableLevelBar[] levels;
+	private GradeBar[] grades;
+
+	public TableBar(String name, TableLevelBar[] levels, GradeBar[] grades) {
+		this.name = name;
+		this.levels = levels;
+		this.grades = grades;
+	}
+
+	@Override
+	public String getTitle() {
+		return name;
+	}
+
+	public TableLevelBar[] getLevels() {
+		return levels;
+	}
+
+	public GradeBar[] getGrades() {
+		return grades;
+	}
+
+}
+
+class TableLevelBar extends Bar {
+	private String level;
+	private String[] hashes;
+
+	public TableLevelBar(String level, String[] hashes) {
+		this.level = level;
+		this.hashes = hashes;
+	}
+
+	@Override
+	public String getTitle() {
+		return "LEVEL " + level;
+	}
+
+	public String[] getHashes() {
+		return hashes;
+	}
+}
+
+class GradeBar extends Bar {
+
+	private SongData[] songs;
+	private String name;
+
+	private IRScoreData score;
+
+	public GradeBar(String name, SongData[] songs) {
+		this.songs = songs;
+		this.name = name;
+	}
+
+	public SongData[] getSongDatas() {
+		return songs;
+	}
+
+	@Override
+	public String getTitle() {
+		return "段位認定 " + name;
+	}
+
+	public boolean existsAllSongs() {
+		for (SongData song : songs) {
+			if (song == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public IRScoreData getMirrorScore() {
+		return score;
+	}
+
+	public void setMirrorScore(IRScoreData score) {
+		this.score = score;
+	}
+
+}
---
+++ src/bms/player/beatoraja/select/BarSorter.java
@@ -0,0 +1,159 @@
+package bms.player.beatoraja.select;
+
+import java.util.Comparator;
+
+/**
+ * バーのソートアルゴリズム
+ * 
+ * @author exch
+ */
+public interface BarSorter extends Comparator<Bar> {
+	/**
+	 * 楽曲名ソート
+	 */
+	public static final BarSorter NAME_SORTER = new NameSorter();
+	/**
+	 * レベルソート
+	 */
+	public static final BarSorter LEVEL_SORTER = new LevelSorter();
+	/**
+	 * スコアレートソート
+	 */
+	public static final BarSorter SCORE_SORTER = new ScoreSorter();
+	/**
+	 * クリアランプソート
+	 */
+	public static final BarSorter LAMP_SORTER = new LampSorter();
+	/**
+	 * ミスカウントソート
+	 */
+	public static final BarSorter MISSCOUNT_SORTER = new MissCountSorter();
+
+	/**
+	 * ソート名を取得する
+	 * 
+	 * @return ソートの名称
+	 */
+	public abstract String getName();
+}
+
+abstract class AbstractBarSorter implements BarSorter {
+	
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
+
+class NameSorter extends AbstractBarSorter {
+
+    public NameSorter() {
+        setName("SONG NAME");
+    }
+
+    @Override
+    public int compare(Bar o1, Bar o2) {
+        if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
+            return 0;
+        }
+        if (!(o1 instanceof SongBar)) {
+            return 1;
+        }
+        if (!(o2 instanceof SongBar)) {
+            return -1;
+        }
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
+
+class LevelSorter extends AbstractBarSorter {
+
+    public LevelSorter() {
+        setName("LEVEL");
+    }
+
+    @Override
+    public int compare(Bar o1, Bar o2) {
+        if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
+            return 0;
+        }
+        if (!(o1 instanceof SongBar)) {
+            return 1;
+        }
+        if (!(o2 instanceof SongBar)) {
+            return -1;
+        }
+        return ((SongBar) o1).getSongData().getLevel() - ((SongBar) o2).getSongData().getLevel();
+    }
+}
+
+class ScoreSorter extends AbstractBarSorter {
+
+    public ScoreSorter() {
+        setName("SCORE RATE");
+    }
+
+    @Override
+    public int compare(Bar o1, Bar o2) {
+        if (o1.getScore() == null && o2.getScore() == null) {
+            return 0;
+        }
+        if (o1.getScore() == null) {
+            return 1;
+        }
+        if (o2.getScore() == null) {
+            return -1;
+        }
+        return o1.getScore().getExscore() * 1000 / o1.getScore().getNotes() - o2.getScore().getExscore()
+                * 1000 / o2.getScore().getNotes();
+    }
+}
+
+class LampSorter extends AbstractBarSorter {
+
+    public LampSorter() {
+        setName("CLEAR LAMP");
+    }
+
+    @Override
+    public int compare(Bar o1, Bar o2) {
+        if (o1.getScore() == null && o2.getScore() == null) {
+            return 0;
+        }
+        if (o1.getScore() == null) {
+            return 1;
+        }
+        if (o2.getScore() == null) {
+            return -1;
+        }
+        return o1.getScore().getClear() - o2.getScore().getClear();
+    }
+}
+
+class MissCountSorter extends AbstractBarSorter {
+
+    public MissCountSorter() {
+        setName("MISS COUNT");
+    }
+
+    @Override
+    public int compare(Bar o1, Bar o2) {
+        if (o1.getScore() == null && o2.getScore() == null) {
+            return 0;
+        }
+        if (o1.getScore() == null) {
+            return 1;
+        }
+        if (o2.getScore() == null) {
+            return -1;
+        }
+        return o1.getScore().getMinbp() - o2.getScore().getMinbp();
+    }
+}
+
