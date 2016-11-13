3804e61ac642bd0e137c7869d69347fdd825b21b
exch-bms2 <exch-bms2@users.noreply.github.com>
2016-11-13 16:40:02 +0900

    reduce latency of creating TableBar

--- src/bms/player/beatoraja/select/Bar.java
+++ src/bms/player/beatoraja/select/Bar.java
@@ -3,16 +3,12 @@ package bms.player.beatoraja.select;
 import bms.player.beatoraja.*;
 import bms.player.beatoraja.TableData.CourseData;
 import bms.player.beatoraja.song.FolderData;
-import bms.player.beatoraja.song.SQLiteSongDatabaseAccessor;
-import bms.player.beatoraja.song.SongData;
-import bms.player.beatoraja.song.SongDatabaseAccessor;
-import bms.player.beatoraja.song.SongUtils;
+import bms.player.beatoraja.song.*;
 
 import com.badlogic.gdx.graphics.Pixmap;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.logging.Logger;
 
 public abstract class Bar {
 
@@ -337,21 +333,35 @@ class TableBar extends DirectoryBar {
     
     public void setTableData(TableData td) {
     	this.td = td;
+
+    	final long t = System.currentTimeMillis();
 		List<TableLevelBar> levels = new ArrayList<TableLevelBar>();
 		for (String lv : td.getLevel()) {
 			levels.add(new TableLevelBar(selector, lv, td.getHash().get(lv)));
 		}
+
 		this.levels = levels.toArray(new TableLevelBar[levels.size()]);
 		List<GradeBar> l = new ArrayList<GradeBar>();
+		
+		Set<String> hashset = new HashSet<String>();
+		for (CourseData course : td.getCourse()) {
+			for (String hash : course.getHash()) {
+				hashset.add(hash);
+			}
+		}
+		SongData[] songs = selector.getSongDatabase().getSongDatas(hashset.toArray(new String[hashset.size()]));
+		
 		for (CourseData course : td.getCourse()) {
 			List<SongData> songlist = new ArrayList<SongData>();
 			for (String hash : course.getHash()) {
-				SongData[] songs = selector.getSongDatabase().getSongDatas("md5", hash);
-				if (songs.length > 0) {
-					songlist.add(songs[0]);
-				} else {
-					songlist.add(null);
+				SongData song = null;
+				for(SongData sd :songs) {
+					if(hash.equals(sd.getMd5())) {
+						song = sd;
+						break;
+					}
 				}
+				songlist.add(song);
 			}
 
 			l.add(new GradeBar(course.getName(), songlist.toArray(new SongData[0]), course));
