670741d4334113ee801bb3dfe2de68fceddab23c
exch-bms2 <exch-bms2@users.noreply.github.com>
2017-01-19 18:30:01 +0900

    Music Select : bug fix

--- src/bms/player/beatoraja/select/BarSorter.java
+++ src/bms/player/beatoraja/select/BarSorter.java
@@ -8,48 +8,54 @@ import java.util.Comparator;
  * @author exch
  */
 public interface BarSorter extends Comparator<Bar> {
-    /**
-     * 楽曲名ソート
-     */
-    public static final BarSorter NAME_SORTER = new NameSorter();
-    /**
-     * レベルソート
-     */
-    public static final BarSorter LEVEL_SORTER = new LevelSorter();
-    /**
-     * スコアレートソート
-     */
-    public static final BarSorter SCORE_SORTER = new ScoreSorter();
-    /**
-     * クリアランプソート
-     */
-    public static final BarSorter LAMP_SORTER = new LampSorter();
-    /**
-     * ミスカウントソート
-     */
-    public static final BarSorter MISSCOUNT_SORTER = new MissCountSorter();
+	/**
+	 * 楽曲名ソート
+	 */
+	public static final BarSorter NAME_SORTER = new NameSorter();
+	/**
+	 * レベルソート
+	 */
+	public static final BarSorter LEVEL_SORTER = new LevelSorter();
+	/**
+	 * スコアレートソート
+	 */
+	public static final BarSorter SCORE_SORTER = new ScoreSorter();
+	/**
+	 * クリアランプソート
+	 */
+	public static final BarSorter LAMP_SORTER = new LampSorter();
+	/**
+	 * ミスカウントソート
+	 */
+	public static final BarSorter MISSCOUNT_SORTER = new MissCountSorter();
 
-    /**
-     * ソート名を取得する
-     *
-     * @return ソートの名称
-     */
-    public abstract String getName();
+	/**
+	 * ソート名を取得する
+	 *
+	 * @return ソートの名称
+	 */
+	public abstract String getName();
+	
+	public static final BarSorter[] ALL = { NAME_SORTER, LEVEL_SORTER, LAMP_SORTER, SCORE_SORTER, MISSCOUNT_SORTER };
+
+	public static BarSorter[] getAllSorter() {
+		return ALL;
+	}
 }
 
 abstract class AbstractBarSorter implements BarSorter {
-    /**
-     * ソート名称
-     */
-    private String name;
+	/**
+	 * ソート名称
+	 */
+	private String name;
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
 }
 
@@ -60,23 +66,23 @@ abstract class AbstractBarSorter implements BarSorter {
  */
 class NameSorter extends AbstractBarSorter {
 
-    public NameSorter() {
-        setName("SONG NAME");
-    }
+	public NameSorter() {
+		setName("SONG NAME");
+	}
 
-    @Override
-    public int compare(Bar o1, Bar o2) {
-        if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
-            return 0;
-        }
-        if (!(o1 instanceof SongBar)) {
-            return 1;
-        }
-        if (!(o2 instanceof SongBar)) {
-            return -1;
-        }
-        return o1.getTitle().compareTo(o2.getTitle());
-    }
+	@Override
+	public int compare(Bar o1, Bar o2) {
+		if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
+			return 0;
+		}
+		if (!(o1 instanceof SongBar)) {
+			return 1;
+		}
+		if (!(o2 instanceof SongBar)) {
+			return -1;
+		}
+		return o1.getTitle().compareTo(o2.getTitle());
+	}
 }
 
 /**
@@ -86,23 +92,23 @@ class NameSorter extends AbstractBarSorter {
  */
 class LevelSorter extends AbstractBarSorter {
 
-    public LevelSorter() {
-        setName("LEVEL");
-    }
+	public LevelSorter() {
+		setName("LEVEL");
+	}
 
-    @Override
-    public int compare(Bar o1, Bar o2) {
-        if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
-            return 0;
-        }
-        if (!(o1 instanceof SongBar)) {
-            return 1;
-        }
-        if (!(o2 instanceof SongBar)) {
-            return -1;
-        }
-        return ((SongBar) o1).getSongData().getLevel() - ((SongBar) o2).getSongData().getLevel();
-    }
+	@Override
+	public int compare(Bar o1, Bar o2) {
+		if (!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) {
+			return 0;
+		}
+		if (!(o1 instanceof SongBar)) {
+			return 1;
+		}
+		if (!(o2 instanceof SongBar)) {
+			return -1;
+		}
+		return ((SongBar) o1).getSongData().getLevel() - ((SongBar) o2).getSongData().getLevel();
+	}
 }
 
 /**
@@ -112,24 +118,25 @@ class LevelSorter extends AbstractBarSorter {
  */
 class ScoreSorter extends AbstractBarSorter {
 
-    public ScoreSorter() {
-        setName("SCORE RATE");
-    }
+	public ScoreSorter() {
+		setName("SCORE RATE");
+	}
 
-    @Override
-    public int compare(Bar o1, Bar o2) {
-        if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) || (o1.getScore() == null && o2.getScore() == null)) {
-            return 0;
-        }
-        if (!(o1 instanceof SongBar) || o1.getScore() == null || o1.getScore().getNotes() == 0) {
-            return 1;
-        }
-        if (!(o2 instanceof SongBar) || o2.getScore() == null || o2.getScore().getNotes() == 0) {
-            return -1;
-        }
-        return o1.getScore().getExscore() * 1000 / o1.getScore().getNotes() - o2.getScore().getExscore()
-                * 1000 / o2.getScore().getNotes();
-    }
+	@Override
+	public int compare(Bar o1, Bar o2) {
+		if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar))
+				|| (o1.getScore() == null && o2.getScore() == null)) {
+			return 0;
+		}
+		if (!(o1 instanceof SongBar) || o1.getScore() == null || o1.getScore().getNotes() == 0) {
+			return 1;
+		}
+		if (!(o2 instanceof SongBar) || o2.getScore() == null || o2.getScore().getNotes() == 0) {
+			return -1;
+		}
+		return o1.getScore().getExscore() * 1000 / o1.getScore().getNotes()
+				- o2.getScore().getExscore() * 1000 / o2.getScore().getNotes();
+	}
 }
 
 /**
@@ -139,23 +146,24 @@ class ScoreSorter extends AbstractBarSorter {
  */
 class LampSorter extends AbstractBarSorter {
 
-    public LampSorter() {
-        setName("CLEAR LAMP");
-    }
+	public LampSorter() {
+		setName("CLEAR LAMP");
+	}
 
-    @Override
-    public int compare(Bar o1, Bar o2) {
-        if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) || (o1.getScore() == null && o2.getScore() == null)) {
-            return 0;
-        }
-        if (!(o1 instanceof SongBar) || o1.getScore() == null) {
-            return 1;
-        }
-        if (!(o2 instanceof SongBar) || o2.getScore() == null) {
-            return -1;
-        }
-        return o1.getScore().getClear() - o2.getScore().getClear();
-    }
+	@Override
+	public int compare(Bar o1, Bar o2) {
+		if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar))
+				|| (o1.getScore() == null && o2.getScore() == null)) {
+			return 0;
+		}
+		if (!(o1 instanceof SongBar) || o1.getScore() == null) {
+			return 1;
+		}
+		if (!(o2 instanceof SongBar) || o2.getScore() == null) {
+			return -1;
+		}
+		return o1.getScore().getClear() - o2.getScore().getClear();
+	}
 }
 
 /**
@@ -165,22 +173,22 @@ class LampSorter extends AbstractBarSorter {
  */
 class MissCountSorter extends AbstractBarSorter {
 
-    public MissCountSorter() {
-        setName("MISS COUNT");
-    }
+	public MissCountSorter() {
+		setName("MISS COUNT");
+	}
 
-    @Override
-    public int compare(Bar o1, Bar o2) {
-        if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar)) || (o1.getScore() == null && o2.getScore() == null)) {
-            return 0;
-        }
-        if (!(o1 instanceof SongBar) || o1.getScore() == null) {
-            return 1;
-        }
-        if (!(o2 instanceof SongBar) || o2.getScore() == null) {
-            return -1;
-        }
-        return o1.getScore().getMinbp() - o2.getScore().getMinbp();
-    }
+	@Override
+	public int compare(Bar o1, Bar o2) {
+		if ((!(o1 instanceof SongBar) && !(o2 instanceof SongBar))
+				|| (o1.getScore() == null && o2.getScore() == null)) {
+			return 0;
+		}
+		if (!(o1 instanceof SongBar) || o1.getScore() == null) {
+			return 1;
+		}
+		if (!(o2 instanceof SongBar) || o2.getScore() == null) {
+			return -1;
+		}
+		return o1.getScore().getMinbp() - o2.getScore().getMinbp();
+	}
 }
-
