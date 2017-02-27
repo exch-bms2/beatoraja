b4f6dac287174713f574fffcbe2eda8253d78b05
exch-bms2 <exch-bms2@users.noreply.github.com>
2017-02-27 21:21:06 +0900

    fixes importing lr2font problem

--- src/bms/player/beatoraja/skin/LR2FontLoader.java
+++ src/bms/player/beatoraja/skin/LR2FontLoader.java
@@ -3,17 +3,10 @@ package bms.player.beatoraja.skin;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.*;
-import java.util.logging.Logger;
-
-import bms.player.beatoraja.Config;
-import bms.player.beatoraja.MainState;
-import bms.player.beatoraja.play.bga.BGAProcessor;
-import bms.player.beatoraja.play.bga.FFmpegProcessor;
-import bms.player.beatoraja.play.bga.MovieProcessor;
-import bms.player.beatoraja.play.bga.VLCMovieProcessor;
-import bms.player.beatoraja.skin.LR2SkinHeader.CustomFile;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.GdxRuntimeException;
@@ -24,83 +17,148 @@ import com.badlogic.gdx.utils.GdxRuntimeException;
  * @author exch
  */
 public class LR2FontLoader extends LR2SkinLoader {
+	/**
+	 * 読み込んだTextureリスト
+	 */
+	private List<Texture> imagelist = new ArrayList<Texture>();
+	/**
+	 * 生成するテキストイメージソース
+	 */
+	private SkinTextImage.SkinTextImageSource textimage;
+	/**
+	 * lr2fontファイルパス
+	 */
+	private Path path;
 
-    private List<Texture> imagelist = new ArrayList<Texture>();
+	public LR2FontLoader() {
+		// size
+		addCommandWord(new CommandWord("S") {
+			@Override
+			public void execute(String[] str) {
+				textimage.setSize(Integer.parseInt(str[1]));
+			}
+		});
+		// margin
+		addCommandWord(new CommandWord("M") {
+			@Override
+			public void execute(String[] str) {
+				textimage.setMargin(Integer.parseInt(str[1]));
+			}
+		});
+		// texture
+		addCommandWord(new CommandWord("T") {
+			@Override
+			public void execute(String[] str) {
+				File imagefile = path.getParent().resolve(str[2]).toFile();
+				// System.out.println("Font image loading : " +
+				// imagefile.getPath());
+				if (imagefile.exists()) {
+					try {
+						// File cim = new File(imagefile.getPath().substring(0,
+						// imagefile.getPath().lastIndexOf('.')) + ".cim");
+						// if(cim.exists()) {
+						// long l = System.nanoTime();
+						// imagefile = cim;
+						// Pixmap pixmap =
+						// PixmapIO.readCIM(Gdx.files.internal(imagefile.getPath()));
+						// imagelist.add(new Texture(pixmap));
+						// System.out.println(imagefile.getPath() + " -> " +
+						// (System.nanoTime() - l));
+						// pixmap.dispose();
+						// } else {
+						// Pixmap pixmap = new
+						// Pixmap(Gdx.files.internal(imagefile.getPath()));
+						// PixmapIO.writeCIM(Gdx.files.absolute(imagefile.getAbsolutePath().substring(0,
+						// imagefile.getPath().lastIndexOf('.')) + ".cim"),
+						// pixmap);
+						// long l = System.nanoTime();
+						// imagelist.add(new Texture(pixmap));
+						// System.out.println(imagefile.getPath() + " -> " +
+						// (System.nanoTime() - l));
+						// pixmap.dispose();
+						// }
+						// ファイル読み込みが最も処理時間が長い(80%)
+						Pixmap pixmap = new Pixmap(Gdx.files.internal(imagefile.getPath()));
+						imagelist.add(new Texture(pixmap));
+						pixmap.dispose();
 
-    private SkinTextImage.SkinTextImageSource textimage;
+					} catch (GdxRuntimeException e) {
+						imagelist.add(null);
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		// reference
+		addCommandWord(new CommandWord("R") {
+			@Override
+			public void execute(String[] str) {
+				try {
+					int[] values = parseInt(str);
+					if (values[2] < imagelist.size() && imagelist.get(values[2]) != null) {
+						// System.out.println("Font loaded : " + values[1]);
+						textimage.setImage(mapCode(values[1]), new TextureRegion(imagelist.get(values[2]), values[3],
+								values[4], values[5], values[6]));
+					}
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
-    private Path path;
+	}
 
-    public LR2FontLoader() {
+	private int mapCode(int code) {
+		int sjiscode = code;
+		byte[] sjisbyte;
+		if (code >= 128) {
+			sjiscode = (char) (code + 32832);
+			sjisbyte = new byte[2];
+			sjisbyte[1] = (byte) (sjiscode & 0xff);
+			sjisbyte[0] = (byte) ((sjiscode & 0xff00) >> 8);
+		} else {
+			sjisbyte = new byte[1];
+			sjisbyte[0] = (byte) (sjiscode & 0xff);
+		}
 
-        addCommandWord(new CommandWord("S") {
-            @Override
-            public void execute(String[] str) {
-                textimage.setSize(Integer.parseInt(str[1]));
-            }
-        });
-        addCommandWord(new CommandWord("M") {
-            @Override
-            public void execute(String[] str) {
-                textimage.setMargin(Integer.parseInt(str[1]));
-            }
-        });
-        addCommandWord(new CommandWord("T") {
-            @Override
-            public void execute(String[] str) {
-                File imagefile = path.getParent().resolve(str[2]).toFile();
-//                System.out.println("Font image loading : " + imagefile.getPath());
-                if (imagefile.exists()) {
-                    try {
-                        imagelist.add(new Texture(Gdx.files.internal(imagefile.getPath())));
-                    } catch (GdxRuntimeException e) {
-                        imagelist.add(null);
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
+		try {
+			byte[] b = new String(sjisbyte, "Shift_JIS").getBytes("utf-16le");
+			int utfcode = 0;
+			for (int i = 0; i < b.length; i++) {
+				utfcode |= (b[i] & 0xff) << (8 * i);
+			}
+			return utfcode;
+		} catch (UnsupportedEncodingException e) {
+		}
+		return 0;
+	}
 
-        addCommandWord(new CommandWord("R") {
-            @Override
-            public void execute(String[] str) {
-                try {
-                    int[] values = parseInt(str);
-                    if (values[2] < imagelist.size() && imagelist.get(values[2]) != null) {
-//                        System.out.println("Font loaded : " + values[1]);
-                        textimage.getImages()[values[1]] = new TextureRegion(imagelist.get(values[2]),values[3], values[4], values[5], values[6]);
-                    }
-                } catch (Throwable e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+	protected SkinTextImage.SkinTextImageSource loadFont(Path p) throws IOException {
+		textimage = new SkinTextImage.SkinTextImageSource();
+		this.path = p;
 
-    }
+		long l = System.nanoTime();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(p.toFile()), "MS932"));
+		String line;
+		while ((line = br.readLine()) != null) {
+			processLine(line, null);
+		}
+		br.close();
 
-    protected SkinTextImage.SkinTextImageSource loadFont(Path p) throws IOException {
-        textimage = new SkinTextImage.SkinTextImageSource();
-        this.path = p;
+		System.out.println(p.toString() + " -> " + (System.nanoTime() - l));
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(p.toFile()), "MS932"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            processLine(line, null);
-        }
-        br.close();
+		return textimage;
+	}
 
-        return textimage;
-    }
+	protected int[] parseInt(String[] s) {
+		int[] result = new int[22];
+		for (int i = 1; i < s.length; i++) {
+			try {
+				result[i] = Integer.parseInt(s[i].replace('!', '-').replaceAll(" ", ""));
+			} catch (Exception e) {
 
-    protected int[] parseInt(String[] s) {
-        int[] result = new int[22];
-        for (int i = 1; i < s.length; i++) {
-            try {
-                result[i] = Integer.parseInt(s[i].replace('!', '-').replaceAll(" ", ""));
-            } catch (Exception e) {
-
-            }
-        }
-        return result;
-    }
+			}
+		}
+		return result;
+	}
 }
--- src/bms/player/beatoraja/skin/SkinTextImage.java
+++ src/bms/player/beatoraja/skin/SkinTextImage.java
@@ -1,21 +1,17 @@
 package bms.player.beatoraja.skin;
 
 import bms.player.beatoraja.MainState;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 
 import java.io.UnsupportedEncodingException;
-
-import static bms.player.beatoraja.skin.SkinProperty.NUMBER_SCRATCHANGLE_1P;
-import static bms.player.beatoraja.skin.SkinProperty.NUMBER_SCRATCHANGLE_2P;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * テキストオブジェクト
@@ -24,137 +20,155 @@ import static bms.player.beatoraja.skin.SkinProperty.NUMBER_SCRATCHANGLE_2P;
  */
 public class SkinTextImage extends SkinObject {
 
-    private SkinTextImageSource source;
+	private SkinTextImageSource source;
 
-    private int align = ALIGN_LEFT;
-    public static final int ALIGN_LEFT = 0;
-    public static final int ALIGN_CENTER = 1;
-    public static final int ALIGN_RIGHT = 2;
+	private int align = ALIGN_LEFT;
+	public static final int ALIGN_LEFT = 0;
+	public static final int ALIGN_CENTER = 1;
+	public static final int ALIGN_RIGHT = 2;
 
-    public static final int[] ALIGN = {Align.left, Align.center, Align.right};
+	public static final int[] ALIGN = { Align.left, Align.center, Align.right };
 
-    private int id = -1;
+	private int id = -1;
 
-    private String text;
-    private String encodetext;
+	private String text;
 
-    public SkinTextImage(SkinTextImageSource source) {
-        this.source = source;
-    }
+	public SkinTextImage(SkinTextImageSource source) {
+		this.source = source;
+	}
 
-    public int getAlign() {
-        return align;
-    }
+	public int getAlign() {
+		return align;
+	}
 
-    public void setAlign(int align) {
-        this.align = align;
-    }
+	public void setAlign(int align) {
+		this.align = align;
+	}
 
-    public void setText(String text) {
-        if(text == null || text.length() == 0) {
-            text = " ";
-        }
-    }
+	public void setText(String text) {
+		if (text == null || text.length() == 0) {
+			text = " ";
+		}
+	}
 
-    public void draw(SpriteBatch sprite, long time, MainState state) {
-        if(id == -1) {
-            return;
-        }
-        final String value = state.getTextValue(id);
-        if(value == null || value.length() == 0) {
-            return;
-        }
-        if(!value.equals(text)) {
-            try {
-                encodetext = new String(value.getBytes("Shift_JIS"), "Shift_JIS");
-                text = value;
-            } catch (UnsupportedEncodingException e) {
-            }
-        }
-        Rectangle r = this.getDestination(time,state);
-        if(r != null && encodetext != null) {
-            Color c = getColor(time,state);
-            final float x = (align == 2 ? r.x - r.width : (align == 1 ? r.x - r.width / 2 : r.x));
-            drawText(sprite, encodetext, align, r ,c);
-        }
-    }
+	public void draw(SpriteBatch sprite, long time, MainState state) {
+		if (id == -1) {
+			return;
+		}
+		final String value = state.getTextValue(id);
+		if (value == null || value.length() == 0) {
+			return;
+		}
+		Rectangle r = this.getDestination(time, state);
+		if (r != null) {
+			Color c = getColor(time, state);
+			final float x = (align == 2 ? r.x - r.width : (align == 1 ? r.x - r.width / 2 : r.x));
+			drawText(sprite, value, align, r, c);
+		}
+	}
 
-    public void draw(SpriteBatch sprite, long time, MainState state, String value, int offsetX, int offsetY) {
-        Rectangle r = this.getDestination(time,state);
-        if(r != null) {
-            Color c = getColor(time,state);
-            drawText(sprite, value, align, r, c);
-        }
-    }
+	public void draw(SpriteBatch sprite, long time, MainState state, String value, int offsetX, int offsetY) {
+		Rectangle r = this.getDestination(time, state);
+		if (r != null) {
+			Color c = getColor(time, state);
+			drawText(sprite, value, align, r, c);
+		}
+	}
 
-    private void drawText(SpriteBatch sprite, String text, int align, Rectangle r, Color c) {
-        float width = 0;
-//        System.out.println("SkinTextImage描画:" + text + " - " + x + " " + y + " " + w + " " + h);
-        for(int i = 0, len = text.length();i < len;i++) {
-            final TextureRegion ch = source.getImages()[text.charAt(i)];
-            if(ch != null) {
-                width += ch.getRegionWidth() * r.height / source.getSize() + source.getMargin();
-            }
-        }
-        final float scale = r.width < width ? r.width / width : 1;
-        final float x = (align == 2 ? r.x - (r.width - width * scale) : (align == 1 ? r.x - width * scale / 2 : r.x));
-        float dx = 0;
-        for(int i = 0, len = text.length();i < len;i++) {
-            final TextureRegion ch = source.getImages()[text.charAt(i)];
-            if(ch != null) {
-                final float tw = ch.getRegionWidth() * scale * r.height / source.getSize();
-//                System.out.println("SkinTextImage描画:" + text.charAt(i) + " - " + (x + dx) + " " + y + " " + tw + " " + h);
-                draw(sprite, ch, x + dx, r.y, tw, r.height, c, 0);
-                dx += tw + source.getMargin() * scale;
-            } else {
-                System.out.println("undefined char : " + text.charAt(i));
-            }
-        }
-    }
+	private void drawText(SpriteBatch sprite, String text, int align, Rectangle r, Color c) {
+		try {
+			// System.out.println("SkinTextImage描画:" + text + " - " + x + " " +
+			// y +
+			// " " + w + " " + h);
+			float width = 0;
+			byte[] b = text.getBytes("utf-16le");
+			for (int i = 0; i < b.length;) {
+				int code = 0;
+				code |= (b[i++] & 0xff);
+				code |= (b[i++] & 0xff) << 8;
+				if (code >= 0xdc00) {
+					code |= (b[i++] & 0xff) << 16;
+					code |= (b[i++] & 0xff) << 24;
+				}
+				final TextureRegion ch = source.getImage(code);
+				if (ch != null) {
+					width += ch.getRegionWidth() * r.height / source.getSize() + source.getMargin();
+				}
+			}
 
-    public void dispose() {
-        source.dispose();
-    }
+			final float scale = r.width < width ? r.width / width : 1;
+			final float x = (align == 2 ? r.x - (r.width - width * scale)
+					: (align == 1 ? r.x - width * scale / 2 : r.x));
+			float dx = 0;
+			for (int i = 0; i < b.length;) {
+				int code = 0;
+				code |= (b[i++] & 0xff);
+				code |= (b[i++] & 0xff) << 8;
+				if (code >= 0xdc00) {
+					code |= (b[i++] & 0xff) << 16;
+					code |= (b[i++] & 0xff) << 24;
+				}
+				final TextureRegion ch = source.getImage(code);
+				if (ch != null) {
+					final float tw = ch.getRegionWidth() * scale * r.height / source.getSize();
+					// System.out.println("SkinTextImage描画:" + text.charAt(i) +
+					// " -
+					// " + (x + dx) + " " + y + " " + tw + " " + h);
+					draw(sprite, ch, x + dx, r.y, tw, r.height, c, 0);
+					dx += tw + source.getMargin() * scale;
+				} else {
+					// System.out.println("undefined char : " + text.charAt(i));
+				}
+			}
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
-    public void setReferenceID(int id) {
-        this.id = id;
-    }
+	public void dispose() {
+		source.dispose();
+	}
 
+	public void setReferenceID(int id) {
+		this.id = id;
+	}
 
-    public static class SkinTextImageSource implements Disposable{
+	public static class SkinTextImageSource implements Disposable {
 
-        private int size = 0;
-        private int margin = 0;
-        private TextureRegion[] images = new TextureRegion[65536];
+		private int size = 0;
+		private int margin = 0;
+		private Map<Integer, TextureRegion> images = new TreeMap<Integer, TextureRegion>();
 
-        public int getMargin() {
-            return margin;
-        }
+		public int getMargin() {
+			return margin;
+		}
 
-        public void setMargin(int margin) {
-            this.margin = margin;
-        }
+		public void setMargin(int margin) {
+			this.margin = margin;
+		}
 
-        public int getSize() {
-            return size;
-        }
+		public int getSize() {
+			return size;
+		}
 
-        public void setSize(int size) {
-            this.size = size;
-        }
+		public void setSize(int size) {
+			this.size = size;
+		}
 
-        public TextureRegion[] getImages() {
-            return images;
-        }
+		public TextureRegion getImage(int index) {
+			return images.get(index);
+		}
 
-        @Override
-        public void dispose() {
-            for(int i = 0;i < images.length;i++) {
-                if(images[i] != null) {
-                    images[i].getTexture().dispose();
-                    images[i] = null;
-                }
-            }
-        }
-    }
+		public void setImage(int index, TextureRegion tex) {
+			images.put(index, tex);
+		}
+
+		@Override
+		public void dispose() {
+			for (TextureRegion tr : images.values()) {
+				tr.getTexture().dispose();
+			}
+		}
+	}
 }
