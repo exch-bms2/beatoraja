7fb225b0ad76bd46f432c553e0616241c96100fc
parataxia <parataxia@github.com>
2017-07-07 22:02:14 -0700

    edited all launch scripts for potential performance increase of >20%.

--- beatoraja-config.bat
+++ beatoraja-config.bat
@@ -1,1 +1,5 @@
+REM *** Set specific UTF-8 encoding for config files etc to UTF-8. Note this is important because sometimes .json files do NOT save correctly otherwise resulting in corrupted files! ***
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8 
+REM *** Set system-wide "_JAVA_OPTIONS" environment variable to use OpenGL pipeline (improved performance of > 30% potentially. Also use anti-aliasing for non-LR2 fonts, and finally allow Swing framework to utilize AA and GTKLookAndFeel for config window. ***
+set _JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 java -Xms1g -Xmx4g -jar beatoraja.jar -c
--- beatoraja-config.command
+++ beatoraja-config.command
@@ -1,3 +1,5 @@
 #!/bin/bash
+export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF-8
+export _JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
 cd "$(dirname "$0")"
 exec java -Xms1g -Xmx4g -jar beatoraja.jar -c
