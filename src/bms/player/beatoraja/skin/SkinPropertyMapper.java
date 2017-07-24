0b3f42442741ee28a6d4c60f200d00f988336891
excln <excln@users.noreply.github.com>
2017-07-25 08:12:01 +0900

    fix SkinPropertyMapper

--- src/bms/player/beatoraja/skin/SkinPropertyMapper.java
+++ src/bms/player/beatoraja/skin/SkinPropertyMapper.java
@@ -7,7 +7,7 @@ public class SkinPropertyMapper {
 	public static int bombTimerId(int player, int key) {
 		if (player < 2) {
 			if (key < 10) {
-				return VALUE_JUDGE_1P_SCRATCH + key + player * 10;
+				return TIMER_BOMB_1P_SCRATCH + key + player * 10;
 			} else if (key < 100) {
 				return TIMER_BOMB_1P_KEY10 + key - 10 + player * 100;
 			}
@@ -18,7 +18,7 @@ public class SkinPropertyMapper {
 	public static int holdTimerId(int player, int key) {
 		if (player < 2) {
 			if (key < 10) {
-				return VALUE_JUDGE_1P_SCRATCH + key + player * 10;
+				return TIMER_HOLD_1P_SCRATCH + key + player * 10;
 			} else if (key < 100) {
 				return TIMER_HOLD_1P_KEY10 + key - 10 + player * 100;
 			}
@@ -29,7 +29,7 @@ public class SkinPropertyMapper {
 	public static int keyOnTimerId(int player, int key) {
 		if (player < 2) {
 			if (key < 10) {
-				return VALUE_JUDGE_1P_SCRATCH + key + player * 10;
+				return TIMER_KEYON_1P_SCRATCH + key + player * 10;
 			} else if (key < 100) {
 				return TIMER_KEYON_1P_KEY10 + key - 10 + player * 100;
 			}
@@ -40,7 +40,7 @@ public class SkinPropertyMapper {
 	public static int keyOffTimerId(int player, int key) {
 		if (player < 2) {
 			if (key < 10) {
-				return VALUE_JUDGE_1P_SCRATCH + key + player * 10;
+				return TIMER_KEYOFF_1P_SCRATCH + key + player * 10;
 			} else if (key < 100) {
 				return TIMER_KEYOFF_1P_KEY10 + key - 10 + player * 100;
 			}
@@ -52,6 +52,22 @@ public class SkinPropertyMapper {
 	public static boolean isKeyJudgeValueId(int valueId) {
 		return (valueId >= VALUE_JUDGE_1P_SCRATCH && valueId <= VALUE_JUDGE_2P_KEY9)
 				|| (valueId >= VALUE_JUDGE_1P_KEY10 && valueId <= VALUE_JUDGE_2P_KEY99);
+	}
+
+	public static int getKeyJudgeValuePlayer(int valueId) {
+		if (valueId >= VALUE_JUDGE_1P_SCRATCH && valueId <= VALUE_JUDGE_2P_KEY9) {
+			return (valueId - VALUE_JUDGE_1P_SCRATCH) / 10;
+		} else {
+			return (valueId - VALUE_JUDGE_1P_KEY10) / 100;
+		}
+	}
+
+	public static int getKeyJudgeValueOffset(int valueId) {
+		if (valueId >= VALUE_JUDGE_1P_SCRATCH && valueId <= VALUE_JUDGE_2P_KEY9) {
+			return (valueId - VALUE_JUDGE_1P_SCRATCH) % 10;
+		} else {
+			return (valueId - VALUE_JUDGE_1P_KEY10) % 100 + 10;
+		}
 	}
 
 	public static int keyJudgeValueId(int player, int key) {
