e093f84ed9303927390b3b85eccd476aff514b50
Leegunmin <dudrjsxo3@naver.com>
2018-03-30 00:14:21 +0900

    # WARNING: head commit changed in the meantime
    test

---
+++ src/bms/player/test/MainControllerTest.java
@@ -0,0 +1,14 @@
+package bms.player.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
