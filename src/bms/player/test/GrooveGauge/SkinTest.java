c4f982cec26f56cdaf5c8046a159c968a79da2c5
HweongWoo <maewakka123@naver.com>
2018-04-02 23:07:23 +0900

    Update SkinTest.java

--- src/bms/player/test/GrooveGauge/SkinTest.java
+++ src/bms/player/test/GrooveGauge/SkinTest.java
@@ -61,7 +61,7 @@ import javafx.stage.Stage;
 public class SkinTest extends MainLoader{
 	Skin skin = new Skin(Resolution.HD,Resolution.HD,fixop);
 	boolean usecim=false;
-	 File imagefile = new File("C:/Users/rnwhd/Desktop/programing/git/beatoraja/skin/default/POMYU Chara/Off/dummy.chp");
+	 File imagefile = new File("C:/Users/형우/eclipse-TestCase/beatoraja/skin/default/POMYU Chara/Off/dummy.chp");
 	int type=0, color=0,side=0,dsttimer=0;
 	int dstOp1=0,dstOp2=0,dstOp3=0,dstOffset=0;
 	float dstx=0.0f,dsty=0.0f,dsth=0.0f,dstw=0.0f;
@@ -93,28 +93,164 @@ public class SkinTest extends MainLoader{
 		dsth=0.0f;
 		dstw=0.0f;
 	}
+	
 	@Test
-	public void typeBoundaryTest(){
-		imagefile = new File("./skin/default/POMYU Chara/Off/dummy.chp");
+	public void typeBoundaryTest_case1(){
+		//Check if type is out of bind -> 16
+		imagefile = new File("C:/Users/형우/eclipse-TestCase/beatoraja/skin/default/POMYU Chara/Off/dummy.chp");
 		type = 16;
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void typeBoundaryTest_case2(){
+		//Check if type is out of bind -> -1
+		imagefile = new File("C:/Users/형우/eclipse-TestCase/beatoraja/skin/default/POMYU Chara/Off/dummy.chp");
 		type = -1;
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void typeBoundaryTest_case3(){
+		//Check if type is not out of bind -> 3
+		imagefile = new File("C:/Users/형우/eclipse-TestCase/beatoraja/skin/default/POMYU Chara/Off/dummy.chp");
 		type = 3;
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
 	}
+	
 	@Test
-	public void fileExistTest(){
-		imagefile = new File("./skin/default/POMYU Chara/Off/dummy.chp");
+	public void fileExistTest_case1(){
+		//Whether or not a chp file exists in the directory
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex1.chp");
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
-		
-		imagefile = new File("./skin/default/POMYU Chara/Off/dummy2.chp");
+	}
+	
+	@Test
+	public void fileExistTest_case2(){
+		//Whether or not a chp file exists in the directory
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex1.chp");
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
-		
+	}
+	
+	@Test
+	public void fileExistTest_case3(){
+		//Confirm when file path address ends with /
 		imagefile = new File("./skin/default/POMYU Chara/Off/");
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
-		
+	}
+	
+	@Test
+	public void fileExistTest_case4(){
+		//When there is no chp file in the file path
 		imagefile = new File("./skin/default/POMYU Chara/Off");
 		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
 	}
+	@Test
+	public void chpFileTest_case1() {
+		//Check first String in CHP FIle is "#CharBMP"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex1.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+		
+	}
+	
+	@Test
+	public void chpFileTest_case2() {
+		//Check first String in CHP FIle is "#CharBMP2P"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex2.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+		
+	}
+	
+	@Test
+	public void chpFileTest_case3() {
+		//Check first String in CHP FIle is "#CharTex"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex3.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case4() {
+		//Check first String in CHP FIle is "#CharTex2P"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex4.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case5() {
+		//Check first String in CHP FIle is "#CharFace"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex5.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case6() {
+		//Check first String in CHP FIle is "#CharFace2P"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex6.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case7() {
+		//Check first String in CHP FIle is "#SelectCG"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex7.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case8() {
+		//Check first String in CHP FIle is "#CharTex2P"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex8.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case9() {
+		//Check first String in CHP FIle is "#SelectCG2P"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex9.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case10() {
+		//Check first String in CHP FIle is "#Patern"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex10.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case11() {
+		//Check first String in CHP FIle is "#Texture"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex11.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case12() {
+		//Check first String in CHP FIle is "#Layer"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex12.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case13() {
+		//Check first String in CHP FIle is "#Flame"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex13.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case14() {
+		//Check first String in CHP FIle is "#Anime"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex14.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+	
+	@Test
+	public void chpFileTest_case15() {
+		//Check first String in CHP FIle is "#Size"
+		imagefile = new File("./skin/default/POMYU Chara/Off/ex15.chp");
+		assertTrue(skin.PMcharaLoader(usecim, imagefile, type, color, dstx, dsty, dstw, dsth, side, dsttimer, dstOp1, dstOp2, dstOp3, dstOffset)==null);
+	}
+
 }
+
