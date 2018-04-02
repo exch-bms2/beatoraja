ecb31bb68470b20e9e66d65184f60f9622647d34
Dong pin Oh <dhehdqls@gmail.com>
2018-04-02 10:32:34 +0900

    Add files via upload

--- src/bms/player/test/AutoplayModifierTest.java
+++ src/bms/player/test/AutoplayModifierTest.java
@@ -3,6 +3,8 @@ package bms.player.test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -12,14 +14,18 @@ import org.junit.Test;
 
 import bms.model.BMSDecoder;
 import bms.model.BMSModel;
+import bms.model.Note;
+import bms.model.TimeLine;
 import bms.player.beatoraja.pattern.AutoplayModifier;
+import bms.player.beatoraja.pattern.LaneShuffleModifier;
+import bms.player.beatoraja.pattern.PatternModifyLog;
 
 public class AutoplayModifierTest {
 	static BMSModel bmsModel;
 	static BMSDecoder bmsDecoder;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		File bmsFile = new File("C:\\Users\\dhehd\\Desktop\\_laika_24.bmson");
+		File bmsFile = new File("C:\\Users\\dhehd\\Desktop\\end_time_dpnep.bms");
 		bmsDecoder = new BMSDecoder();
 		bmsModel = bmsDecoder.decode(bmsFile);
 	}
@@ -52,13 +58,36 @@ public class AutoplayModifierTest {
 	public void modifyTest() {
 		int lanes[] = {0, 1};
 		AutoplayModifier autoplayModifier = new AutoplayModifier(lanes);
+		Note[] before = getNote(bmsModel);
 		autoplayModifier.modify(bmsModel);
+		assertNotEquals(before, getNote(bmsModel));
+		
 	}
 	
 	@Test
 	public void modifyWithMargin() {
 		int lanes[] = {0, 1};
 		AutoplayModifier autoplayModifier = new AutoplayModifier(lanes,1);
+		Note[] before = getNote(bmsModel);
 		autoplayModifier.modify(bmsModel);
+		assertNotEquals(before, getNote(bmsModel));
+	}
+	
+	
+	public Note[] getNote(BMSModel model) {
+		List<PatternModifyLog> log = new ArrayList();
+		int lanes = model.getMode().key;
+		TimeLine[] timelines = model.getAllTimeLines();
+		for (int index = 0; index < timelines.length; index++) {
+			final TimeLine tl = timelines[index];
+			if (tl.existNote() || tl.existHiddenNote()) {
+				Note[] notes = new Note[lanes];
+				for (int i = 0; i < lanes; i++) {
+					notes[i] = tl.getNote(i);
+					return notes;
+				}
+			}
+		}
+		return null;
 	}
 }
