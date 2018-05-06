d8ee297225e88abc4d170c6e80f80eb9004bf97c
Leegunmin <dudrjsxo3@naver.com>
2018-05-06 22:44:11 +0900

    1. Extract Class 2. PMcharaLoader.java PlayCharacter method 3. Play할 때 Skin character를 load하는 부분에서 timer를 setting 하는 것은 맞지 않다고 생각하여 extract class를 하여 timer를 setting 할 수 있도록 SkinTimer class를 만들어서 extract calss를 해주었다.

--- src/bms/player/beatoraja/skin/PMcharaLoader.java
+++ src/bms/player/beatoraja/skin/PMcharaLoader.java
@@ -181,35 +181,8 @@ public class PMcharaLoader {
 						if(data.size() > i + 2) dst[i] = data.get(i + 2).replaceAll("[^0-9a-zA-Z-]", "");
 					}
 					int timer = Integer.MIN_VALUE;
-					if(setMotion != Integer.MIN_VALUE && setMotion == motion) {
-						timer = dsttimer;
-					} else if(setMotion == Integer.MIN_VALUE) {
-						if(side != 2) {
-							if(motion == 1) timer = TIMER_PM_CHARA_1P_NEUTRAL;
-							else if(motion == 6) timer = TIMER_PM_CHARA_1P_FEVER;
-							else if(motion == 7) timer = TIMER_PM_CHARA_1P_GREAT;
-							else if(motion == 8) timer = TIMER_PM_CHARA_1P_GOOD;
-							else if(motion == 10) timer = TIMER_PM_CHARA_1P_BAD;
-							else if(motion >= 15 && motion <= 17) {
-								timer = TIMER_MUSIC_END;
-								if(motion == 15) {
-									skinOption.setDstOpt1(OPTION_1P_BORDER_OR_MORE);
-									skinOption.setDstOpt2( -OPTION_1P_100);
-								}
-								else if(motion == 16) skinOption.setDstOpt1(-OPTION_1P_BORDER_OR_MORE);	//LOSE
-								else if(motion == 17) skinOption.setDstOpt1(OPTION_1P_100);	//FEVERWIN
-							}
-						} else {
-							if(motion == 1) timer = TIMER_PM_CHARA_2P_NEUTRAL;
-							else if(motion == 7) timer = TIMER_PM_CHARA_2P_GREAT;
-							else if(motion == 10) timer = TIMER_PM_CHARA_2P_BAD;
-							else if(motion == 15 || motion == 16) {
-								timer = TIMER_MUSIC_END;
-								if(motion == 15) skinOption.setDstOpt1(-OPTION_1P_BORDER_OR_MORE);	//WIN
-								else if(motion == 16) skinOption.setDstOpt1( OPTION_1P_BORDER_OR_MORE);	//LOSE
-							}
-						}
-					}
+					SkinTimer skinTimer = new SkinTimer(timer, setMotion);
+					skinTimer.TimerSetting(motion, skinOption, side ,dsttimer);
 					if(timer != Integer.MIN_VALUE
 							&& (dst[0].length() > 0 && dst[0].length() % 2 == 0)
 							&& (dst[1].length() == 0 || (dst[1].length() > 0 && dst[1].length() == dst[0].length()))
