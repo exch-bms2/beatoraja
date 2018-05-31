bfbf6f205d374023c769b2d9058ef1dd8e174f0c
exch-bms2 <exch-bms2@users.noreply.github.com>
2018-05-31 19:55:27 +0900

    IRConnection : add comment, fix minor problem

--- src/bms/player/beatoraja/ir/IRConnection.java
+++ src/bms/player/beatoraja/ir/IRConnection.java
@@ -21,76 +21,117 @@ import bms.player.beatoraja.song.SongData;
  * @author exch
  */
 public interface IRConnection {
-	
+
+	/**
+	 * IRに新規ユーザー登録する。
+	 * 
+	 * @param id
+	 *            ユーザーID
+	 * @param pass
+	 *            パスワード
+	 * @param name
+	 *            ユーザー名
+	 * @return
+	 */
 	public IRResponse<Object> register(String id, String pass, String name);
 
 	/**
 	 * IRにログインする。起動時に呼び出される
-	 * @param id ユーザーID
-	 * @param pass パスワード
+	 * 
+	 * @param id
+	 *            ユーザーID
+	 * @param pass
+	 *            パスワード
 	 */
 	public IRResponse<Object> login(String id, String pass);
 
 	/**
 	 * ライバルデータを収録する
+	 * 
 	 * @return ライバルデータ
 	 */
 	public IRResponse<PlayerInformation[]> getRivals();
 
 	/**
 	 * IRに設定されている表データを収録する
+	 * 
 	 * @return IRで取得可能な表データ
 	 */
 	public IRResponse<TableData[]> getTableDatas();
 
 	/**
 	 * スコアデータを取得する
-	 * @param id ユーザーID。譜面に登録されているスコアデータを全取得する場合はnullを入れる
-	 * @param model スコアデータを取得する譜面。ユーザーIDのスコアデータを全取得する場合はnullを入れる
+	 * 
+	 * @param id
+	 *            ユーザーID。譜面に登録されているスコアデータを全取得する場合はnullを入れる
+	 * @param model
+	 *            スコアデータを取得する譜面。ユーザーIDのスコアデータを全取得する場合はnullを入れる
 	 * @return
 	 */
 	public IRResponse<IRScoreData[]> getPlayData(String id, BMSModel model);
-	
+
 	public IRResponse<IRScoreData[]> getCoursePlayData(String id, CourseData course, int lnmode);
 
 	/**
 	 * スコアデータを送信する
+	 * 
 	 * @param model
+	 *            楽曲データ
 	 * @param score
+	 *            スコア
+	 * @return 送信結果
 	 */
 	public IRResponse<Object> sendPlayData(BMSModel model, IRScoreData score);
+
 	/**
-	 * スコアデータを送信する
-	 * @param model
+	 * コーススコアデータを送信する
+	 * 
+	 * @param course
+	 *            コースデータ
+	 * @param lnmode
+	 *            LNモード
 	 * @param score
+	 *            スコア
+	 * @return 送信結果
 	 */
 	public IRResponse<Object> sendCoursePlayData(CourseData course, int lnmode, IRScoreData score);
-	
+
 	/**
 	 * 楽曲のURLを取得する
-	 * @param song 譜面データ
-	 * @return
+	 * 
+	 * @param song
+	 *            譜面データ
+	 * @return 楽曲URL。存在しない場合はnull
 	 */
 	public String getSongURL(SongData song);
 
 	/**
 	 * コースのURLを取得する
-	 * @param course コースデータ
-	 * @return
+	 * 
+	 * @param course
+	 *            コースデータ
+	 * @return コースURL。存在しない場合はnull
 	 */
 	public String getCourseURL(CourseData course);
 
 	/**
 	 * プレイヤーURLを取得する
-	 * @param id ユーザーID
+	 * 
+	 * @param id
+	 *            ユーザーID
 	 * @return
 	 */
 	public String getPlayerURL(String id);
-	
+
+	/**
+	 * 利用可能な全てのIRConnectionの名称を返す
+	 * 
+	 * @return IRConnectionの名称
+	 */
 	public static String[] getAllAvailableIRConnectionName() {
 		Class[] irclass = getAllAvailableIRConnection();
 		String[] names = new String[irclass.length];
-		for(int i = 0;i < names.length;i++) {
+		for (int i = 0; i < names.length; i++) {
 			try {
 				names[i] = irclass[i].getField("NAME").get(null).toString();
 			} catch (Throwable e) {
@@ -99,12 +140,22 @@ public interface IRConnection {
 		}
 		return names;
 	}
-	
+
+	/**
+	 * 名称に対応したIRConnectionインスタンスを返す
+	 * 
+	 * @param name
+	 *            IRCOnnectionの名称
+	 * @return 対応するIRConnectionインスタンス。存在しない場合はnull
+	 */
 	public static IRConnection getIRConnection(String name) {
+		if (name == null || name.length() == 0) {
+			return null;
+		}
 		Class[] irclass = getAllAvailableIRConnection();
-		for(int i = 0;i < irclass.length;i++) {
+		for (int i = 0; i < irclass.length; i++) {
 			try {
-				if(name.equals(irclass[i].getField("NAME").get(null).toString())) {
+				if (name.equals(irclass[i].getField("NAME").get(null).toString())) {
 					return (IRConnection) irclass[i].newInstance();
 				}
 			} catch (Throwable e) {
@@ -113,69 +164,70 @@ public interface IRConnection {
 		}
 		return null;
 	}
-	
+
 	static Class[] getAllAvailableIRConnection() {
 		List<Class> classes = new ArrayList();
 
 		ClassLoader cl = Thread.currentThread().getContextClassLoader();
 		try {
 			Enumeration<URL> urls = cl.getResources("bms/player/beatoraja/ir");
-			while(urls.hasMoreElements()) {
-				URL url  = urls.nextElement();
-				if(url.getProtocol().equals("jar")) {
-			        JarURLConnection jarUrlConnection = (JarURLConnection)url.openConnection();
-			        JarFile jarFile = null;
-			        try {
-			            jarFile = jarUrlConnection.getJarFile();
-			            Enumeration<JarEntry> jarEnum = jarFile.entries();
+			while (urls.hasMoreElements()) {
+				URL url = urls.nextElement();
+				if (url.getProtocol().equals("jar")) {
+					JarURLConnection jarUrlConnection = (JarURLConnection) url.openConnection();
+					JarFile jarFile = null;
+					try {
+						jarFile = jarUrlConnection.getJarFile();
+						Enumeration<JarEntry> jarEnum = jarFile.entries();
 
-			            while (jarEnum.hasMoreElements()) {
-			                JarEntry jarEntry = jarEnum.nextElement();
-			                String path = jarEntry.getName();
-			                if (path.startsWith("bms/player/beatoraja/ir/") && path.endsWith(".class")) {
-			                    Class c = cl.loadClass("bms.player.beatoraja.ir." + path.substring(path.lastIndexOf("/") + 1, path.length() - 6));
-								for(Class inf : c.getInterfaces()) {
-									if(inf == IRConnection.class) {
-										for(Field f : c.getFields()) {
-											if(f.getName().equals("NAME")) {
+						while (jarEnum.hasMoreElements()) {
+							JarEntry jarEntry = jarEnum.nextElement();
+							String path = jarEntry.getName();
+							if (path.startsWith("bms/player/beatoraja/ir/") && path.endsWith(".class")) {
+								Class c = cl.loadClass("bms.player.beatoraja.ir."
+										+ path.substring(path.lastIndexOf("/") + 1, path.length() - 6));
+								for (Class inf : c.getInterfaces()) {
+									if (inf == IRConnection.class) {
+										for (Field f : c.getFields()) {
+											if (f.getName().equals("NAME")) {
 												Object irname = c.getField("NAME").get(null);
-												classes.add(c);										
+												classes.add(c);
 											}
 										}
 										break;
 									}
 								}
-			                }
-			            }
-			        } finally {
-			            if (jarFile != null) {
-			                jarFile.close();
-			            }
-			        }
+							}
+						}
+					} finally {
+						if (jarFile != null) {
+							jarFile.close();
+						}
+					}
 				}
-				if(url.getProtocol().equals("file")) {
+				if (url.getProtocol().equals("file")) {
 					File dir = new File(url.getPath());
-		            for (String path : dir.list()) {
-		                if (path.endsWith(".class")) {
-		                    Class c = cl.loadClass("bms.player.beatoraja.ir." + path.substring(0, path.length() - 6));
-							for(Class inf : c.getInterfaces()) {
-								if(inf == IRConnection.class) {
-									for(Field f : c.getFields()) {
-										if(f.getName().equals("NAME")) {
+					for (String path : dir.list()) {
+						if (path.endsWith(".class")) {
+							Class c = cl.loadClass("bms.player.beatoraja.ir." + path.substring(0, path.length() - 6));
+							for (Class inf : c.getInterfaces()) {
+								if (inf == IRConnection.class) {
+									for (Field f : c.getFields()) {
+										if (f.getName().equals("NAME")) {
 											Object irname = c.getField("NAME").get(null);
-											classes.add(c);										
+											classes.add(c);
 										}
 									}
 									break;
 								}
 							}
-		                }
-		            }
+						}
+					}
 				}
 			}
 		} catch (Throwable e) {
 			e.printStackTrace();
-		}		
+		}
 		return classes.toArray(new Class[classes.size()]);
 	}
 }
