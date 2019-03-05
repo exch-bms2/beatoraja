e3e603e5eb9fab152120bbd16e8493742d30050b
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-03-05 17:41:54 +0900

    MusicResult : add OPEN_RANKING_ON_IR command

---
+++ src/bms/player/beatoraja/result/MusicResultCommand.java
@@ -0,0 +1,32 @@
+package bms.player.beatoraja.result;
+
+import bms.player.beatoraja.ir.IRConnection;
+
+import java.awt.*;
+import java.net.URI;
+
+public enum MusicResultCommand {
+
+    OPEN_RANKING_ON_IR {
+        @Override
+        public void execute(MusicResult result) {
+            IRConnection ir = result.main.getIRStatus().length > 0 ? result.main.getIRStatus()[0].connection : null;
+            if(ir == null) {
+                return;
+            }
+
+            String url = ir.getSongURL(result.main.getPlayerResource().getSongdata());
+            if (url != null) {
+                try {
+                    URI uri = new URI(url);
+                    Desktop.getDesktop().browse(uri);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    public abstract void execute(MusicResult result);
+
+}
