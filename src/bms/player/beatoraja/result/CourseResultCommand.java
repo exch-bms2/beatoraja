74cdbac845a646a5db4838e6e6101ecc6c175fb4
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-03-07 22:09:40 +0900

    CourseResult : add OPEN_RANKING_ON_IR command

---
+++ src/bms/player/beatoraja/result/CourseResultCommand.java
@@ -0,0 +1,32 @@
+package bms.player.beatoraja.result;
+
+import bms.player.beatoraja.ir.IRConnection;
+
+import java.awt.*;
+import java.net.URI;
+
+public enum CourseResultCommand {
+
+    OPEN_RANKING_ON_IR {
+        @Override
+        public void execute(CourseResult result) {
+            IRConnection ir = result.main.getIRStatus().length > 0 ? result.main.getIRStatus()[0].connection : null;
+            if(ir == null) {
+                return;
+            }
+
+            String url = ir.getCourseURL(result.main.getPlayerResource().getCourseData());
+            if (url != null) {
+                try {
+                    URI uri = new URI(url);
+                    Desktop.getDesktop().browse(uri);
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    public abstract void execute(CourseResult result);
+
+}
