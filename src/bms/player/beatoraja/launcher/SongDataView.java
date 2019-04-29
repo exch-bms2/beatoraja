dbb0ddac234bed0e080136904940bcb6e5e2eb6a
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-04-29 15:39:56 +0900

    SongDataView : add column, setVisible method

--- src/bms/player/beatoraja/launcher/SongDataView.java
+++ src/bms/player/beatoraja/launcher/SongDataView.java
@@ -1,6 +1,8 @@
 package bms.player.beatoraja.launcher;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 import bms.player.beatoraja.song.SongData;
@@ -16,20 +18,44 @@ public class SongDataView implements Initializable {
 	@FXML
 	private TableColumn<SongData, String> artist;
 	@FXML
+	private TableColumn<SongData, String> genre;
+	@FXML
 	private TableColumn<SongData, Integer> mode;
 	@FXML
 	private TableColumn<SongData, Integer> notes;
+	@FXML
+	private TableColumn<SongData, Integer> level;
+	@FXML
+	private TableColumn<SongData, String> sha256;
+	
+	private Map<String, TableColumn> columnMap = new HashMap<String, TableColumn>();
 
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// タイトル
-		title.setCellValueFactory(new PropertyValueFactory<SongData, String>("fullTitle"));
-//		title.setCellFactory((arg) -> {
-//			return new TableCell<SongData, String>() {
-//
-//			};
-//		});
-		artist.setCellValueFactory(new PropertyValueFactory<SongData, String>("fullArtist"));
-		mode.setCellValueFactory(new PropertyValueFactory<SongData, Integer>("mode"));
-		notes.setCellValueFactory(new PropertyValueFactory<SongData, Integer>("notes"));
+		initColumn(title, "fullTitle");
+		initColumn(artist, "fullArtist");
+		initColumn(genre, "genre");
+		initColumn(mode, "mode");
+		initColumn(notes, "notes");
+		initColumn(level, "level");
+		initColumn(sha256, "sha256");
+	}
+	
+	private void initColumn(TableColumn column, String value) {
+		column.setCellValueFactory(new PropertyValueFactory(value));		
+		columnMap.put(value, column);
+	}
+	
+	public void setVisible(String... values) {
+		for(TableColumn column : columnMap.values()) {
+			column.setVisible(false);
+		}
+		
+		for(String value : values) {
+			TableColumn column = columnMap.get(value);
+			if(column != null) {
+				column.setVisible(true);;
+			}
+		}
 	}
 }
