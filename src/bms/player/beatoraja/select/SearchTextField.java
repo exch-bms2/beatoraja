722b03242d380834e428c10abf904d3ed57e5e8d
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-05-18 00:07:20 +0900

    SearchTextField : fix crash about loading font

--- src/bms/player/beatoraja/select/SearchTextField.java
+++ src/bms/player/beatoraja/select/SearchTextField.java
@@ -2,6 +2,9 @@ package bms.player.beatoraja.select;
 
 import bms.player.beatoraja.Resolution;
 import bms.player.beatoraja.select.bar.SearchWordBar;
+
+import java.util.logging.Logger;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,6 +16,7 @@ import com.badlogic.gdx.scenes.scene2d.*;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField.TextFieldListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
@@ -37,76 +41,80 @@ public class SearchTextField extends Stage {
 
 		final Rectangle r = ((MusicSelectSkin) selector.getSkin()).getSearchTextRegion();
 
-		generator = new FreeTypeFontGenerator(Gdx.files.internal("skin/default/VL-Gothic-Regular.ttf"));
-		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-		parameter.size = (int) r.height;
-		searchfont = generator.generateFont(parameter);
+		try {
+			generator = new FreeTypeFontGenerator(Gdx.files.internal("skin/default/VL-Gothic-Regular.ttf"));
+			FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+			parameter.size = (int) r.height;
+			searchfont = generator.generateFont(parameter);
 
-		final TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle(); // background
-		textFieldStyle.font = searchfont;
-		textFieldStyle.fontColor = Color.WHITE;
-		textFieldStyle.cursor = new TextureRegionDrawable(new TextureRegion(new Texture("skin/default/system.png"), 0, 8, 8, 8));
-		textFieldStyle.selection = new TextureRegionDrawable(new TextureRegion(new Texture("skin/default/system.png"), 0, 8, 2, 8));
-		textFieldStyle.messageFont = searchfont;
-		textFieldStyle.messageFontColor = Color.GRAY;
+			final TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle(); // background
+			textFieldStyle.font = searchfont;
+			textFieldStyle.fontColor = Color.WHITE;
+			textFieldStyle.cursor = new TextureRegionDrawable(new TextureRegion(new Texture("skin/default/system.png"), 0, 8, 8, 8));
+			textFieldStyle.selection = new TextureRegionDrawable(new TextureRegion(new Texture("skin/default/system.png"), 0, 8, 2, 8));
+			textFieldStyle.messageFont = searchfont;
+			textFieldStyle.messageFontColor = Color.GRAY;
 
-		search = new TextField("", textFieldStyle);
-		search.setMessageText("search song");
-		search.setTextFieldListener(new TextFieldListener() {
+			search = new TextField("", textFieldStyle);
+			search.setMessageText("search song");
+			search.setTextFieldListener(new TextFieldListener() {
 
-			public void keyTyped(TextField textField, char key) {
-				if (key == '\n' || key == 13) {
-					if (textField.getText().length() > 0) {
-						SearchWordBar swb = new SearchWordBar(selector, textField.getText());
-						int count = swb.getChildren().length;
-						if (count > 0) {
-							selector.getBarRender().addSearch(swb);
-							selector.getBarRender().updateBar(null);
-							selector.getBarRender().setSelected(swb);
-							textField.setText("");
-							textField.setMessageText(count + " song(s) found");
-							textFieldStyle.messageFontColor = Color.valueOf("00c0c0");
-						} else {
-							selector.main.getInputProcessor().setEnterPressed(false);
-							textField.setText("");
-							textField.setMessageText("no song found");
-							textFieldStyle.messageFontColor = Color.DARK_GRAY;
+				public void keyTyped(TextField textField, char key) {
+					if (key == '\n' || key == 13) {
+						if (textField.getText().length() > 0) {
+							SearchWordBar swb = new SearchWordBar(selector, textField.getText());
+							int count = swb.getChildren().length;
+							if (count > 0) {
+								selector.getBarRender().addSearch(swb);
+								selector.getBarRender().updateBar(null);
+								selector.getBarRender().setSelected(swb);
+								textField.setText("");
+								textField.setMessageText(count + " song(s) found");
+								textFieldStyle.messageFontColor = Color.valueOf("00c0c0");
+							} else {
+								selector.main.getInputProcessor().setEnterPressed(false);
+								textField.setText("");
+								textField.setMessageText("no song found");
+								textFieldStyle.messageFontColor = Color.DARK_GRAY;
+							}
 						}
+						textField.getOnscreenKeyboard().show(false);
+						setKeyboardFocus(null);
 					}
-					textField.getOnscreenKeyboard().show(false);
-					setKeyboardFocus(null);
-				}
-				if (!searchfont.getData().hasGlyph(key)) {
-					FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-					parameter.size = (int) r.height;
-					parameter.characters += textField.getText() + key;
-					BitmapFont newsearchfont = generator.generateFont(parameter);
-					textFieldStyle.font = newsearchfont;
-					textFieldStyle.messageFont = newsearchfont;
-					searchfont.dispose();
-					searchfont = newsearchfont;
-					textField.appendText(String.valueOf(key));
+					if (!searchfont.getData().hasGlyph(key)) {
+						FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+						parameter.size = (int) r.height;
+						parameter.characters += textField.getText() + key;
+						BitmapFont newsearchfont = generator.generateFont(parameter);
+						textFieldStyle.font = newsearchfont;
+						textFieldStyle.messageFont = newsearchfont;
+						searchfont.dispose();
+						searchfont = newsearchfont;
+						textField.appendText(String.valueOf(key));
+					}
+
 				}
 
-			}
+			});
+			search.setBounds(r.x, r.y, r.width, r.height);
+			search.setMaxLength(50);
+			search.setFocusTraversal(false);
+			addActor(search);
 
-		});
-		search.setBounds(r.x, r.y, r.width, r.height);
-		search.setMaxLength(50);
-		search.setFocusTraversal(false);
-		addActor(search);
-
-		search.setVisible(true);
-		search.addListener(new EventListener() {
-			@Override
-			public boolean handle(Event e) {
-				if (e.isHandled()) {
-					selector.main.getInputProcessor().getKeyBoardInputProcesseor()
-							.setEnable(getKeyboardFocus() == null);
+			search.setVisible(true);
+			search.addListener(new EventListener() {
+				@Override
+				public boolean handle(Event e) {
+					if (e.isHandled()) {
+						selector.main.getInputProcessor().getKeyBoardInputProcesseor()
+								.setEnable(getKeyboardFocus() == null);
+					}
+					return false;
 				}
-				return false;
-			}
-		});
+			});			
+		} catch (GdxRuntimeException e) {
+			Logger.getGlobal().warning("Search Text読み込み失敗");
+		}
 	}
 
 	public void dispose() {
