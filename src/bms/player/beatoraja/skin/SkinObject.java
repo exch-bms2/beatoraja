c71eac220624d527f59d02de0bc6cdbaa21054dc
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-08-13 01:49:56 +0900

    SkinObject : add getOffsets()

--- src/bms/player/beatoraja/skin/SkinObject.java
+++ src/bms/player/beatoraja/skin/SkinObject.java
@@ -734,6 +734,10 @@ public abstract class SkinObject implements Disposable {
 			this.off = new SkinOffset[this.offset.length];
 		}
 	}
+	
+	public SkinOffset[] getOffsets() {
+		return off;
+	}
 
 	public TimerProperty getDestinationTimer() {
 		return dsttimer;
