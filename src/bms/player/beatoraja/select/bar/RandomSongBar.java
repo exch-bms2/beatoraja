adfae07ad0850cba1053e5aedaaca579b4a973bf
SackMagiclight <20876249+SackMagiclight@users.noreply.github.com>
2019-09-05 21:46:04 +0900

    fix format

--- src/bms/player/beatoraja/select/bar/RandomSongBar.java
+++ src/bms/player/beatoraja/select/bar/RandomSongBar.java
@@ -14,52 +14,52 @@ public class RandomSongBar extends SelectableBar {
 	private MainState state;
 	private Queue<Integer> queue;
 	private SongData currentSong = null;
-	
+
 	public RandomSongBar(SongData[] songs, MainState state) {
 		super();
 		this.songs = songs;
 		this.state = state;
 		createIndexQueue();
 	}
-	
+
 	public SongData getSongData() {
 		return _getSongData();
-    }
-	
+	}
+
 	private synchronized SongData _getSongData() {
-		if(queue.size() == 0) {
+		if (queue.size() == 0) {
 			createIndexQueue();
 		}
-		
-		if(state instanceof MusicSelector || currentSong == null) {
+
+		if (state instanceof MusicSelector || currentSong == null) {
 			int index = queue.remove();
 			currentSong = songs[index];
 		}
-		
+
 		return currentSong;
 	}
-	
+
 	private void createIndexQueue() {
 		queue = new ArrayDeque<>();
-		for(int i = 0; i < QueueLength - 1; i++) {
-			int index = ((int)(Math.random()* (songs.length)));
+		for (int i = 0; i < QueueLength - 1; i++) {
+			int index = ((int) (Math.random() * (songs.length)));
 			queue.add(index);
 		}
 	}
-	
-	@Override
-    public String getTitle() {
-        return "RANDOM SELECT";
-    }
 
-    @Override
-    public String getArtist() {
-        return "";
-    }
-    
-    @Override
-    public int getLamp(boolean isPlayer) {
-        return 0;
-    }
+	@Override
+	public String getTitle() {
+		return "RANDOM SELECT";
+	}
+
+	@Override
+	public String getArtist() {
+		return "";
+	}
+
+	@Override
+	public int getLamp(boolean isPlayer) {
+		return 0;
+	}
 
 }
