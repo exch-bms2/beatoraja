6a2f12996a3330d1d8c60a6f1cda4aa19b0fb8b1
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-10-14 00:18:16 +0900

    SkinBPMGraph : add updateGraph(SongInformation)

--- src/bms/player/beatoraja/skin/SkinBPMGraph.java
+++ src/bms/player/beatoraja/skin/SkinBPMGraph.java
@@ -4,13 +4,12 @@ import bms.model.*;
 import bms.player.beatoraja.MainState;
 import bms.player.beatoraja.skin.Skin.SkinObjectRenderer;
 import bms.player.beatoraja.song.SongData;
+import bms.player.beatoraja.song.SongInformation;
 
 import java.util.*;
 
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.utils.IntArray;
 
 /**
  * BPM推移のグラフ
@@ -43,6 +42,11 @@ public class SkinBPMGraph extends SkinObject {
 	private Color otherLineColor = Color.valueOf("ffff00");			//黄
 	private Color stopLineColor = Color.valueOf("ff00ff");			//紫
 	private Color transitionLineColor = Color.valueOf("7f7f7f");	//灰
+
+	private double[][] data = new double[0][2];
+	private double mainbpm;
+	private double minbpm;
+	private double maxbpm;
 
 	private double minValue = 1d/8;
 	private double maxValue = 8;
@@ -87,11 +91,15 @@ public class SkinBPMGraph extends SkinObject {
 	public void draw(SkinObjectRenderer sprite) {
 		final SongData song = state.main.getPlayerResource().getSongdata();
 		final BMSModel model = song != null ? song.getBMSModel() : null;
-
+		
 		if(current == null || song != current || (this.model == null && model != null) || shapetex == null) {
 			current = song;
 			this.model = model;
-			updateGraph(model);
+			if(song != null && song.getInformation() != null) {
+				updateGraph(song.getInformation());
+			} else {
+				updateGraph(model);
+			}
 		}
 
 		final float render = time >= delay ? 1.0f : (float) time / delay;
@@ -99,9 +107,66 @@ public class SkinBPMGraph extends SkinObject {
 		draw(sprite, shapetex, region.x, region.y + region.height, (int)(region.width * render), -region.height);
 	}
 
+	private void updateGraph(SongInformation info) {
+		data = info.getSpeedchangeValues();
+		minbpm = Double.MAX_VALUE;
+		maxbpm = Double.MIN_VALUE;
+		for(double[] d : data) {
+			if(d[0] > 0) {
+				minbpm = Math.min(d[0], minbpm);				
+			}
+			maxbpm = Math.min(d[0], maxbpm);
+		}
+		this.mainbpm = info.getMainbpm();
+		
+		updateTexture();
+	}
+
 	private void updateGraph(BMSModel model) {
-		Pixmap shape;
 		if (model == null) {
+			data = new double[0][2];
+		} else {
+			List<double[]> speedList = new ArrayList<double[]>();
+			Map<Double, Integer> bpmNoteCountMap = new HashMap<Double, Integer>();
+			double nowSpeed = model.getBpm();
+			speedList.add(new double[] {nowSpeed, 0.0});
+			final TimeLine[] tls = model.getAllTimeLines();
+			for (TimeLine tl : tls) {
+				int notecount = bpmNoteCountMap.containsKey(tl.getBPM()) ? bpmNoteCountMap.get(tl.getBPM()) : 0;
+				bpmNoteCountMap.put(tl.getBPM(), notecount + tl.getTotalNotes());
+
+				if(tl.getStop() > 0) {
+					if(nowSpeed != 0) {
+						nowSpeed = 0;					
+						speedList.add(new double[] {nowSpeed, tl.getTime()});
+					}
+				} else if(nowSpeed != tl.getBPM() * tl.getScroll()) {
+					nowSpeed = tl.getBPM() * tl.getScroll();
+					speedList.add(new double[] {nowSpeed, tl.getTime()});
+				}
+			}
+			
+			int maxcount = 0;
+			for (double bpm : bpmNoteCountMap.keySet()) {
+				if (bpmNoteCountMap.get(bpm) > maxcount) {
+					maxcount = bpmNoteCountMap.get(bpm);
+					mainbpm = bpm;
+				}
+			}
+			if(speedList.get(speedList.size() - 1)[1] != tls[tls.length - 1].getTime()) {
+				speedList.add(new double[] {nowSpeed, tls[tls.length - 1].getTime()});			
+			}
+			
+			data = speedList.toArray(new double[speedList.size()][]);
+			minbpm = model.getMinBPM();
+			maxbpm = model.getMaxBPM();	
+		}
+		updateTexture();
+	}
+	
+	private void updateTexture() {
+		Pixmap shape;
+		if (data.length < 2) {
 			shape = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
 			shapetex = new TextureRegion(new Texture(shape));
 		} else {
@@ -109,76 +174,43 @@ public class SkinBPMGraph extends SkinObject {
 			final int height = (int) Math.abs(region.height);
 			shape = new Pixmap(width, height, Pixmap.Format.RGBA8888);
 
-			double mainBPM = model.getBpm();
-			Map<Double, Integer> bpmNoteCountMap = new HashMap<Double, Integer>();
-			List<Double> BPMList = new ArrayList<Double>();
-			IntArray BPMTimeList = new IntArray();
-			double nowBPM = model.getBpm();
-			BPMList.add(model.getBpm());
-			BPMTimeList.add(0);
-			for (TimeLine tl : model.getAllTimeLines()) {
-				Integer count = bpmNoteCountMap.get(tl.getBPM());
-				if (count == null) {
-					count = 0;
-				}
-				bpmNoteCountMap.put(tl.getBPM(), count + tl.getTotalNotes());
-				if(tl.getStop() > 0) {
-					BPMList.add(0d);
-					BPMTimeList.add(tl.getTime());
-					nowBPM = 0;
-				}
-				if(nowBPM != tl.getBPM()) {
-					BPMList.add(tl.getBPM());
-					BPMTimeList.add(tl.getTime() + tl.getStop());
-					nowBPM = tl.getBPM();
-				}
-			}
-			int maxcount = 0;
-			for (double bpm : bpmNoteCountMap.keySet()) {
-				if (bpmNoteCountMap.get(bpm) > maxcount) {
-					maxcount = bpmNoteCountMap.get(bpm);
-					mainBPM = bpm;
-				}
-			}
-			double minBPM = model.getMinBPM();
-			double maxBPM = model.getMaxBPM();
-			int lastTime = model.getLastTime() + 1000;
+			int lastTime = (int) (data[data.length - 1][1] + 1000);
 
 			// グラフ描画
 			int x1,x2,y1,y2;
-			for (int i = 1; i < BPMList.size(); i++) {
+			for (int i = 1; i < data.length; i++) {
 				//縦線
-				x1 = (int) (width * BPMTimeList.get(i) / lastTime);
-				y1 = (int) ((Math.log10(Math.min(Math.max((BPMList.get(i-1) / mainBPM),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
+				x1 = (int) (width * data[i][1] / lastTime);
+				y1 = (int) ((Math.log10(Math.min(Math.max((data[i - 1][0] / mainbpm),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
 				x2 = x1;
-				y2 = (int) ((Math.log10(Math.min(Math.max((BPMList.get(i) / mainBPM),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
+				y2 = (int) ((Math.log10(Math.min(Math.max((data[i][0] / mainbpm),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
 				if(Math.abs(y2 - y1) - lineWidth > 0) {
 					shape.setColor(transitionLineColor);
 					shape.fillRectangle(x1, Math.min(y1, y2) + lineWidth, lineWidth, Math.abs(y2 - y1) - lineWidth);
 				}
 				//横線
-				x1 = (int) (width * BPMTimeList.get(i-1) / lastTime);
-				y1 = (int) ((Math.log10(Math.min(Math.max((BPMList.get(i-1) / mainBPM),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
-				x2 = (int) (width * BPMTimeList.get(i) / lastTime);
+				x1 = (int) (width * data[i - 1][1] / lastTime);
+				y1 = (int) ((Math.log10(Math.min(Math.max((data[i - 1][0] / mainbpm),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
+				x2 = (int) (width * data[i][1] / lastTime);
 				y2 = y1;
 				Color lineColor = otherLineColor;
-				if(BPMList.get(i-1) == mainBPM) lineColor = mainLineColor;
-				else if(BPMList.get(i-1) == minBPM) lineColor = minLineColor;
-				else if(BPMList.get(i-1) == maxBPM) lineColor = maxLineColor;
-				else if(BPMList.get(i-1) == 0) lineColor = stopLineColor;
+				if(data[i - 1][0] == mainbpm) lineColor = mainLineColor;
+				else if(data[i - 1][0] == minbpm) lineColor = minLineColor;
+				else if(data[i - 1][0] == maxbpm) lineColor = maxLineColor;
+				else if(data[i - 1][0] <= 0) lineColor = stopLineColor;
 				shape.setColor(lineColor);
 				shape.fillRectangle(x1, y2, x2 - x1 + lineWidth, lineWidth);
 			}
 			//横線
-			x1 = (int) (width * BPMTimeList.get(BPMTimeList.size -1) / lastTime);
-			y1 = (int) ((Math.log10(Math.min(Math.max((BPMList.get(BPMTimeList.size -1) / mainBPM),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
+			x1 = (int) (width * data[data.length - 1][1] / lastTime);
+			y1 = (int) ((Math.log10(Math.min(Math.max((data[data.length - 1][0] / mainbpm),minValue),maxValue)) - minValueLog) / (maxValueLog-minValueLog) * (height - lineWidth));
 			x2 = (int) width;
 			y2 = y1;
 			Color lineColor = otherLineColor;
-			if(BPMList.get(BPMList.size()-1) == mainBPM) lineColor = mainLineColor;
-			else if(BPMList.get(BPMList.size()-1) == minBPM) lineColor = minLineColor;
-			else if(BPMList.get(BPMList.size()-1) == maxBPM) lineColor = maxLineColor;
-			else if(BPMList.get(BPMList.size()-1) == 0) lineColor = stopLineColor;
+			if(data[data.length - 1][0] == mainbpm) lineColor = mainLineColor;
+			else if(data[data.length - 1][0] == minbpm) lineColor = minLineColor;
+			else if(data[data.length - 1][0] == maxbpm) lineColor = maxLineColor;
+			else if(data[data.length - 1][0] <= 0) lineColor = stopLineColor;
 			shape.setColor(lineColor);
 			shape.fillRectangle(x1, y2, x2 - x1 + lineWidth, lineWidth);
 			if (shapetex != null) {
@@ -188,7 +220,6 @@ public class SkinBPMGraph extends SkinObject {
 			shapetex = new TextureRegion(new Texture(shape));
 			shape.dispose();
 		}
-
 	}
 
 	@Override
