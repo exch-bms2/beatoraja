ad659bacdb113530cbeb27c464f6f0e66c47329a
exch-bms2 <exch-bms2@users.noreply.github.com>
2019-11-18 21:34:22 +0900

    MineNoteModifier : add ADD_RANDOM, ADD_NEAR, ADD_ALL mode

--- src/bms/player/beatoraja/pattern/MineNoteModifier.java
+++ src/bms/player/beatoraja/pattern/MineNoteModifier.java
@@ -2,30 +2,79 @@ package bms.player.beatoraja.pattern;
 
 import java.util.List;
 
-import bms.model.BMSModel;
-import bms.model.LongNote;
-import bms.model.MineNote;
-import bms.model.NormalNote;
-import bms.model.TimeLine;
+import bms.model.*;
+
+import javax.print.DocFlavor;
+
 
 public class MineNoteModifier extends PatternModifier {
 
 	private boolean exists = false;
-	
+
+	private Mode mode = Mode.REMOVE;
+
+	private int damage = 10;
+
 	public MineNoteModifier() {
-		super(2);
 	}
-	
+
+	public MineNoteModifier(int mode) {
+		this.mode = Mode.values()[mode];
+	}
+
 	@Override
 	public List<PatternModifyLog> modify(BMSModel model) {
-		for (TimeLine tl : model.getAllTimeLines()) {
-			for(int lane = 0;lane < model.getMode().key;lane++) {
-				if(tl.getNote(lane) instanceof MineNote) {
-					exists = true;
-					tl.setNote(lane, null);
+		if(mode == Mode.REMOVE) {
+			AssistLevel assist = AssistLevel.NONE;
+			for (TimeLine tl : model.getAllTimeLines()) {
+				for(int lane = 0;lane < model.getMode().key;lane++) {
+					if(tl.getNote(lane) instanceof MineNote) {
+						assist = AssistLevel.LIGHT_ASSIST;
+						exists = true;
+						tl.setNote(lane, null);
+					}
+				}
+			}
+			setAssistLevel(assist);
+			return null;
+		}
+
+		TimeLine[] tls = model.getAllTimeLines();
+		boolean[] ln = new boolean[model.getMode().key];
+		boolean[] blank = new boolean[model.getMode().key];
+
+		for (int i = 0;i < tls.length;i++) {
+			final TimeLine tl = tls[i];
+
+			for(int key = 0;key < model.getMode().key;key++) {
+				final Note note = tl.getNote(key);
+				if(note instanceof LongNote) {
+					ln[key] = !((LongNote) note).isEnd();
+				}
+				blank[key] = !ln[key] && note == null;
+			}
+
+			for(int key = 0;key < model.getMode().key;key++) {
+				if(blank[key]) {
+					switch (mode) {
+						case ADD_RANDOM:
+							if(Math.random() > 0.9) {
+								tl.setNote(key, new MineNote(-1, damage));
+							}
+							break;
+						case ADD_NEAR:
+							if((key > 0 && !blank[key - 1]) || (key < model.getMode().key - 1 && !blank[key + 1])) {
+								tl.setNote(key, new MineNote(-1, damage));
+							}
+							break;
+						case ADD_BLANK:
+							tl.setNote(key, new MineNote(-1, damage));
+							break;
+					}
 				}
 			}
 		}
+
 		return null;
 	}
 	
@@ -33,4 +82,7 @@ public class MineNoteModifier extends PatternModifier {
 		return exists;
 	}
 
+	public enum Mode {
+		REMOVE, ADD_RANDOM, ADD_NEAR, ADD_BLANK;
+	}
 }
