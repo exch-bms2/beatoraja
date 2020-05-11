1463780ccb95698ed044e580d26aed8c5979016e
exch-bms2 <exch-bms2@users.noreply.github.com>
2020-05-11 21:49:42 +0900

    IRCourseData : add IRTrophyData

--- src/bms/player/beatoraja/ir/IRCourseData.java
+++ src/bms/player/beatoraja/ir/IRCourseData.java
@@ -23,6 +23,10 @@ public class IRCourseData {
      */
     public final CourseDataConstraint[] constraint;
 	/**
+	 * トロフィーデータ
+	 */
+	public final IRTrophyData[] trophy;
+	/**
 	 * LN TYPE(-1 : 未指定, 0: LN, 1: CN, 2: HCN)
 	 */
 	public final int lntype;
@@ -42,5 +46,36 @@ public class IRCourseData {
     		constraint[i] =course.getConstraint()[i];
     	}
     	this.lntype = lntype;
+
+    	this.trophy = new IRTrophyData[course.getTrophy().length];
+    	for(int i = 0; i < trophy.length;i++) {
+    		trophy[i] = new IRTrophyData(course.getTrophy()[i]);
+		}
     }
+
+	/**
+	 * IR用トロフィーデータ
+	 *
+	 * @author exch
+	 */
+	public static class IRTrophyData {
+		/**
+		 * トロフィー名称
+		 */
+    	public final String name;
+		/**
+		 * トロフィーのスコアレート条件
+		 */
+		public final float scorerate;
+		/**
+		 * トロフィーのミスレート条件
+		 */
+		public final float smissrate;
+
+    	public IRTrophyData (CourseData.TrophyData trophy) {
+    		name = trophy.getName();
+    		scorerate = trophy.getScorerate();
+			smissrate = trophy.getMissrate();
+		}
+	}
 }
