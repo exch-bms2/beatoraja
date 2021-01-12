7e41ccd70c31a45ed8700dec8a07aaaa7907cb53
author <author@example.com>
2021-01-12 22:01:43 +0900

    [BugFix]
    Fixed a bug that prevented you from changing the green value in the song
    selection screen

--- src/bms/player/beatoraja/select/MusicSelectCommand.java
+++ src/bms/player/beatoraja/select/MusicSelectCommand.java
@@ -17,6 +17,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import bms.player.beatoraja.PlayConfig;
+import bms.player.beatoraja.PlayModeConfig;
 import bms.player.beatoraja.PlayerConfig;
 import bms.player.beatoraja.PlayerInformation;
 import bms.player.beatoraja.ir.IRChartData;
@@ -545,7 +546,9 @@ public enum MusicSelectCommand {
     DURATION_UP {
 		@Override
 		public void execute(MusicSelector selector) {
-            PlayConfig pc = selector.getSelectedBarPlayConfig();
+	    final PlayerConfig config = selector.main.getPlayerConfig();
+	    PlayModeConfig pmc = config.getPlayConfig(config.getMode());
+	    PlayConfig pc = pmc.getPlayconfig();
             if (pc != null && pc.getDuration() < 5000) {
                 pc.setDuration(pc.getDuration() + 1);
                 selector.play(SOUND_OPTIONCHANGE);
@@ -555,7 +558,9 @@ public enum MusicSelectCommand {
     DURATION_DOWN {
 		@Override
 		public void execute(MusicSelector selector) {
-            PlayConfig pc = selector.getSelectedBarPlayConfig();
+	    final PlayerConfig config = selector.main.getPlayerConfig();
+	    PlayModeConfig pmc = config.getPlayConfig(config.getMode());
+	    PlayConfig pc = pmc.getPlayconfig();
             if (pc != null && pc.getDuration() > 1) {
                 pc.setDuration(pc.getDuration() - 1);
                 selector.play(SOUND_OPTIONCHANGE);
@@ -565,7 +570,9 @@ public enum MusicSelectCommand {
     DURATION_UP_LARGE {
         @Override
         public void execute(MusicSelector selector) {
-            PlayConfig pc = selector.getSelectedBarPlayConfig();
+	    final PlayerConfig config = selector.main.getPlayerConfig();
+	    PlayModeConfig pmc = config.getPlayConfig(config.getMode());
+	    PlayConfig pc = pmc.getPlayconfig();
             if (pc != null && pc.getDuration() < 5000) {
                 int duration = pc.getDuration() + 10;
                 pc.setDuration(duration - duration % 10);
@@ -576,7 +583,9 @@ public enum MusicSelectCommand {
     DURATION_DOWN_LARGE {
         @Override
         public void execute(MusicSelector selector) {
-            PlayConfig pc = selector.getSelectedBarPlayConfig();
+	    final PlayerConfig config = selector.main.getPlayerConfig();
+	    PlayModeConfig pmc = config.getPlayConfig(config.getMode());
+	    PlayConfig pc = pmc.getPlayconfig();
             if (pc != null && pc.getDuration() > 10) {
                 int duration = pc.getDuration() - 10;
                 pc.setDuration(duration - duration % 10);
