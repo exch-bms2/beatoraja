3537f04184cc8ac3fa108b24d9ced0260b848aa5
exch-bms2 <exch-bms2@users.noreply.github.com>
2021-05-12 12:30:07 +0900

    EventFactory : refactor

--- src/bms/player/beatoraja/skin/property/EventFactory.java
+++ src/bms/player/beatoraja/skin/property/EventFactory.java
@@ -18,53 +18,84 @@ public class EventFactory {
 	 * @return イベントオブジェクト
 	 */
 	public static Event getEvent(int eventId) {
-		if (eventId == BUTTON_REPLAY) {
-			return createZeroArgEvent(getReplayEventConsumer(0), eventId);
-		}
-		if (eventId == BUTTON_REPLAY2) {
-			return createZeroArgEvent(getReplayEventConsumer(1), eventId);
-		}
-		if (eventId == BUTTON_REPLAY3) {
-			return createZeroArgEvent(getReplayEventConsumer(2), eventId);
-		}
-		if (eventId == BUTTON_REPLAY4) {
-			return createZeroArgEvent(getReplayEventConsumer(3), eventId);
-		}
-		
-		if (eventId == BUTTON_OPEN_IR_WEBSITE) {
-			return createZeroArgEvent((state) -> {
-				if(state instanceof MusicSelector) {
-					MusicSelectCommand.OPEN_RANKING_ON_IR.execute((MusicSelector) state);
-				}
-				if(state instanceof MusicResult) {
-					MusicResultCommand.OPEN_RANKING_ON_IR.execute((MusicResult) state, true);
-				}
-				if(state instanceof CourseResult) {
-					CourseResultCommand.OPEN_RANKING_ON_IR.execute((CourseResult) state);
-				}
-			}, eventId);			
-		}		
-		if (eventId == BUTTON_FAVORITTE_CHART) {
-			return createOneArgEvent((state, arg1) -> {
-				if(state instanceof MusicSelector) {
-					(arg1 >= 0 ? MusicSelectCommand.NEXT_FAVORITE_CHART : MusicSelectCommand.PREV_FAVORITE_CHART)	.execute((MusicSelector) state);			}
-				if(state instanceof MusicResult) {
-					MusicResultCommand.CHANGE_FAVORITE_CHART.execute((MusicResult) state, arg1 >= 0);
-				}
-			}, eventId);
-		}
-		if (eventId == BUTTON_FAVORITTE_SONG) {
-			return createOneArgEvent((state, arg1) -> {
-				if(state instanceof MusicSelector) {
-					(arg1 >= 0 ? MusicSelectCommand.NEXT_FAVORITE_SONG : MusicSelectCommand.PREV_FAVORITE_SONG).execute((MusicSelector) state);			}
-				if(state instanceof MusicResult) {
-					MusicResultCommand.CHANGE_FAVORITE_SONG.execute((MusicResult) state, arg1 >= 0);
-				}
-			}, eventId);
+		for(EventType t : EventType.values()) {
+			if(t.id == eventId) {
+				return t.event;
+			}
 		}
 
 		// 0～2引数に対応できるように2引数取っておく
 		return createTwoArgEvent((state, arg1, arg2) -> state.executeEvent(eventId, arg1, arg2), eventId);
+	}
+
+	/**
+	 * name指定によるイベント取得(組み込みまたはカスタムイベントとして登録したもの)
+	 * @param eventName イベント名称
+	 * @return イベントオブジェクト
+	 */
+	public static Event getEvent(String eventName) {
+		for(EventType t : EventType.values()) {
+			if(t.name().equals(eventName)) {
+				return t.event;
+			}
+		}
+
+		return null;
+	}
+
+	public enum EventType {
+
+		replay1(19, getReplayEventConsumer(0)),
+		replay2(316, getReplayEventConsumer(1)),
+		replay3(317, getReplayEventConsumer(2)),
+		replay4(318, getReplayEventConsumer(3)),
+
+		open_ir(210, (state) -> {
+			if(state instanceof MusicSelector) {
+				MusicSelectCommand.OPEN_RANKING_ON_IR.execute((MusicSelector) state);
+			}
+			if(state instanceof MusicResult) {
+				MusicResultCommand.OPEN_RANKING_ON_IR.execute((MusicResult) state, true);
+			}
+			if(state instanceof CourseResult) {
+				CourseResultCommand.OPEN_RANKING_ON_IR.execute((CourseResult) state);
+			}
+		}),
+		favorite_chart(90, (state, arg1) -> {
+			if(state instanceof MusicSelector) {
+				(arg1 >= 0 ? MusicSelectCommand.NEXT_FAVORITE_CHART : MusicSelectCommand.PREV_FAVORITE_CHART)	.execute((MusicSelector) state);			}
+			if(state instanceof MusicResult) {
+				MusicResultCommand.CHANGE_FAVORITE_CHART.execute((MusicResult) state, arg1 >= 0);
+			}
+		}),
+		favorite_song(89, (state, arg1) -> {
+			if(state instanceof MusicSelector) {
+				(arg1 >= 0 ? MusicSelectCommand.NEXT_FAVORITE_SONG : MusicSelectCommand.PREV_FAVORITE_SONG).execute((MusicSelector) state);			}
+			if(state instanceof MusicResult) {
+				MusicResultCommand.CHANGE_FAVORITE_SONG.execute((MusicResult) state, arg1 >= 0);
+			}
+		}),
+		;
+
+		/**
+		 * property ID
+		 */
+		private final int id;
+		/**
+		 * StringProperty
+		 */
+		private final Event event;
+
+		private EventType(int id, Consumer<MainState> action) {
+			this.id = id;
+			this.event = createZeroArgEvent(action, id);
+		}
+
+		private EventType(int id, BiConsumer<MainState, Integer> action) {
+			this.id = id;
+			this.event = createOneArgEvent(action, id);
+		}
+
 	}
 	
 	private static Consumer<MainState> getReplayEventConsumer(int index) {
