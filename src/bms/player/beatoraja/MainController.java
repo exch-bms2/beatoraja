8c5349bb44884ff27e3258250869d810ee22b0e2
k4584587 <k4584587@naver.com>
2021-08-17 11:57:13 +0900

    Add: Discord RPC

--- src/bms/player/beatoraja/MainController.java
+++ src/bms/player/beatoraja/MainController.java
@@ -8,6 +8,7 @@ import java.util.*;
 import java.util.logging.Logger;
 import java.util.stream.Stream;
 
+import bms.player.beatoraja.config.Discord;
 import org.lwjgl.input.Mouse;
 
 import com.badlogic.gdx.*;
@@ -124,6 +125,9 @@ public class MainController extends ApplicationAdapter {
 	protected TextureRegion black;
 	protected TextureRegion white;
 
+	public static Discord discord;
+
+
 	public MainController(Path f, Config config, PlayerConfig player, BMSPlayerMode auto, boolean songUpdated) {
 		this.auto = auto;
 		this.config = config;
@@ -175,10 +179,10 @@ public class MainController extends ApplicationAdapter {
 					}
 				}
 			}
-			
+
 		}
 		ir = irarray.toArray(IRStatus.class);
-		
+
 		switch(config.getAudioConfig().getDriver()) {
 		case PortAudio:
 			try {
@@ -229,6 +233,8 @@ public class MainController extends ApplicationAdapter {
 		MainState newState = null;
 		switch (state) {
 		case MUSICSELECT:
+			discord = new Discord("MUSIC SELECT");
+			discord.update();
 			if (this.bmsfile != null) {
 				exit();
 			} else {
@@ -246,6 +252,8 @@ public class MainController extends ApplicationAdapter {
 			newState = bmsplayer;
 			break;
 		case RESULT:
+			discord = new Discord("RESULT");
+			discord.update();
 			newState = result;
 			break;
 		case COURSERESULT:
@@ -266,7 +274,7 @@ public class MainController extends ApplicationAdapter {
 			}
 			newState.create();
 			if(newState.getSkin() != null) {
-				newState.getSkin().prepare(newState);				
+				newState.getSkin().prepare(newState);
 			}
 			current = newState;
 			starttime = System.nanoTime();
@@ -279,7 +287,7 @@ public class MainController extends ApplicationAdapter {
 			Gdx.input.setInputProcessor(input.getKeyBoardInputProcesseor());
 		}
 	}
-	
+
 	public MainState getCurrentState() {
 		return current;
 	}
@@ -299,7 +307,7 @@ public class MainController extends ApplicationAdapter {
 			generator = new FreeTypeFontGenerator(Gdx.files.internal("skin/default/VL-Gothic-Regular.ttf"));
 			FreeTypeFontParameter parameter = new FreeTypeFontParameter();
 			parameter.size = 24;
-			systemfont = generator.generateFont(parameter);			
+			systemfont = generator.generateFont(parameter);
 		} catch (GdxRuntimeException e) {
 			Logger.getGlobal().severe("System Font１読み込み失敗");
 		}
@@ -378,7 +386,7 @@ public class MainController extends ApplicationAdapter {
 			});
 			download.start(null);
 		}
-		
+
 		if(ir.length > 0) {
 			messageRenderer.addMessage(ir.length + " IR Connection Succeed" ,5000, Color.GREEN, 1);
 		}
@@ -628,7 +636,7 @@ public class MainController extends ApplicationAdapter {
 	public MusicDownloadProcessor getMusicDownloadProcessor(){
 		return download;
 	}
-	
+
 	public MessageRenderer getMessageRenderer() {
 		return messageRenderer;
 	}
@@ -742,7 +750,7 @@ public class MainController extends ApplicationAdapter {
 			downloadIpfs.start();
 		}
 	}
-	
+
 	public static String getVersion() {
 		return VERSION;
 	}
@@ -823,7 +831,7 @@ public class MainController extends ApplicationAdapter {
 
 	/**
 	 * BGM、効果音セット管理用クラス
-	 * 
+	 *
 	 * @author exch
 	 */
 	public static class SystemSoundManager {
@@ -846,10 +854,10 @@ public class MainController extends ApplicationAdapter {
 
 		public SystemSoundManager(Config config) {
 			if(config.getBgmpath() != null && config.getBgmpath().length() > 0) {
-				scan(Paths.get(config.getBgmpath()).toAbsolutePath(), bgms, "select.wav");				
+				scan(Paths.get(config.getBgmpath()).toAbsolutePath(), bgms, "select.wav");
 			}
 			if(config.getSoundpath() != null && config.getSoundpath().length() > 0) {
-				scan(Paths.get(config.getSoundpath()).toAbsolutePath(), sounds, "clear.wav");				
+				scan(Paths.get(config.getSoundpath()).toAbsolutePath(), sounds, "clear.wav");
 			}
 			Logger.getGlobal().info("検出されたBGM Set : " + bgms.size + " Sound Set : " + sounds.size);
 		}
@@ -883,16 +891,16 @@ public class MainController extends ApplicationAdapter {
 					}
 				} catch (IOException e) {
 				}
-			} 
+			}
 		}
 	}
 
 	public static class IRStatus {
-		
+
 		public final IRConfig config;
 		public final IRConnection connection;
 		public final IRPlayerData player;
-		
+
 		public IRStatus(IRConfig config, IRConnection connection, IRPlayerData player) {
 			this.config = config;
 			this.connection = connection;
