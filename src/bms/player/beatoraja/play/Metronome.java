ff1b521d80c6644b9435f9569842bacef6714adc
PaulHowell <keikun12@gmail.com>
2021-10-19 15:04:29 +0900

    practiceConfigurationのmetronome設定を反映

--- src/bms/player/beatoraja/play/Metronome.java
+++ src/bms/player/beatoraja/play/Metronome.java
@@ -4,15 +4,20 @@ import bms.player.beatoraja.audio.AudioDriver;
 
 public class Metronome {
 
-	boolean enabled = true;
+	boolean enabled;
 	RhythmTimerProcessor rhythm;
 	AudioDriver audio;
 	int lastSections = 0;
 	int lastQuarterNote = 0;
 
-	public Metronome(BMSPlayer main) {
+	public Metronome(BMSPlayer main, boolean enabled) {
 		this.rhythm = main.getRhythmTimerProcessor();
 		this.audio = main.main.getAudioProcessor();
+		this.enabled = enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
 	}
 
 	// rhythmのアップデート後に呼ぶ
