cfe2de788ae38568013b0cc227eb104fec1bd57d
wcko87 <wcko87@gmail.com>
2022-12-20 00:02:01 +0900

    Discord : Improve Discord RPC text

--- src/bms/player/beatoraja/MainController.java
+++ src/bms/player/beatoraja/MainController.java
@@ -244,7 +244,7 @@ public class MainController extends ApplicationAdapter {
 		MainState newState = null;
 		switch (state) {
 		case MUSICSELECT:
-			discord = new Discord("MUSIC SELECT");
+			discord = new Discord("In Music Select Menu", "");
 			discord.update();
 			if (this.bmsfile != null) {
 				exit();
@@ -263,11 +263,13 @@ public class MainController extends ApplicationAdapter {
 			newState = bmsplayer;
 			break;
 		case RESULT:
-			discord = new Discord("RESULT");
+			discord = new Discord("Result Screen", "");
 			discord.update();
 			newState = result;
 			break;
 		case COURSERESULT:
+			discord = new Discord("Result Screen", "");
+			discord.update();
 			newState = gresult;
 			break;
 		case CONFIG:
