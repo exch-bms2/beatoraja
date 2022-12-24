0dbe8d58caf80e87234a6906abf0e5c94b0f0771
exch-bms2 <exch-bms2@users.noreply.github.com>
2022-12-25 02:38:27 +0900

    move RankingDataCache from MusicSelector to bms.player.beatorraja.ir

--- src/bms/player/beatoraja/select/MusicSelector.java
+++ src/bms/player/beatoraja/select/MusicSelector.java
@@ -3,14 +3,10 @@ package bms.player.beatoraja.select;
 import static bms.player.beatoraja.skin.SkinProperty.*;
 
 import java.nio.file.DirectoryStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.io.File;
-import java.lang.StringBuilder;
 import java.nio.file.*;
 import java.util.logging.Logger;
 
-import bms.player.beatoraja.ir.IRPlayerData;
 import bms.player.beatoraja.play.TargetProperty;
 
 import com.badlogic.gdx.graphics.Color;
@@ -18,11 +14,9 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.*;
 import com.badlogic.gdx.utils.ObjectMap.Keys;
 
-import bms.model.BMSDecoder;
 import bms.model.Mode;
 import bms.player.beatoraja.*;
 import bms.player.beatoraja.Config.SongPreview;
-import bms.player.beatoraja.CourseData.CourseDataConstraint;
 import bms.player.beatoraja.ScoreDatabaseAccessor.ScoreDataCollector;
 import bms.player.beatoraja.external.ScoreDataImporter;
 import bms.player.beatoraja.input.BMSPlayerInputProcessor;
@@ -708,95 +702,6 @@ public class MusicSelector extends MainState {
 		if (value >= 0 && value < 1) {
 			final int rankingMax = currentir != null ? Math.max(1, currentir.getTotalPlayer()) : 1;
 			rankingOffset = (int) (rankingMax * value);
-		}
-	}
-
-	/**
-	 * IRアクセスデータのキャッシュ
-	 *
-	 * @author exch
-	 */
-	public static class RankingDataCache {
-
-	    /**
-	     * IRアクセスデータのキャッシュ
-	     */
-	    private ObjectMap<String, RankingData>[] scorecache;
-	    private ObjectMap<String, RankingData>[] cscorecache;
-
-	    public RankingDataCache() {
-	        scorecache = new ObjectMap[4];
-	        cscorecache = new ObjectMap[4];
-	        for (int i = 0; i < scorecache.length; i++) {
-	            scorecache[i] = new ObjectMap(2000);
-	            cscorecache[i] = new ObjectMap(100);
-	        }
-	    }
-
-	    /**
-	     * 指定した楽曲データ、LN MODEに対するIRアクセスデータを返す
-	     * @param song 楽曲データ
-	     * @param lnmode LN MODE
-	     * @return IRアクセスデータ。存在しない場合はnull
-	     */
-	    public RankingData get(SongData song, int lnmode) {
-	        final int cacheindex = song.hasUndefinedLongNote() ? lnmode : 3;
-	        if (scorecache[cacheindex].containsKey(song.getSha256())) {
-	            return scorecache[cacheindex].get(song.getSha256());
-	        }
-	        return null;
-	    }
-
-	    public RankingData get(CourseData course, int lnmode) {
-	        int cacheindex = 3;
-	        for(SongData song : course.getSong()) {
-	        	if(song.hasUndefinedLongNote()) {
-	        		cacheindex = lnmode;
-	        	}
-	        }
-	        String hash = createCourseHash(course);
-	        if (cscorecache[cacheindex].containsKey(hash)) {
-	            return cscorecache[cacheindex].get(hash);
-	        }
-	        return null;
-	    }
-
-	    public void put(SongData song, int lnmode, RankingData iras) {
-	        final int cacheindex = song.hasUndefinedLongNote() ? lnmode : 3;
-	        scorecache[cacheindex].put(song.getSha256(), iras);
-	    }
-	    
-	    public void put(CourseData course, int lnmode, RankingData iras) {
-	        int cacheindex = 3;
-	        for(SongData song : course.getSong()) {
-	        	if(song.hasUndefinedLongNote()) {
-	        		cacheindex = lnmode;
-	        	}
-	        }
-	        cscorecache[cacheindex].put(createCourseHash(course), iras);
-	    }
-	    
-		private String createCourseHash(CourseData course) {
-			StringBuilder sb = new StringBuilder();
-			for(SongData song : course.getSong()) {
-				if(song.getSha256() != null && song.getSha256().length() == 64) {
-					sb.append(song.getSha256());
-				} else {
-					return null;
-				}
-			}
-			for(CourseDataConstraint constraint : course.getConstraint()) {
-				sb.append(constraint.name);
-			}
-			try {
-				MessageDigest md = MessageDigest.getInstance("sha-256");
-				md.update(sb.toString().getBytes());
-				return BMSDecoder.convertHexString(md.digest());
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return null;
 		}
 	}
 }
