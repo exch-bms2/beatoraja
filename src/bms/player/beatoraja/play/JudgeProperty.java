f98940ac2e68b8b2d17ee41db029343256136252
solitarywalker <17350000+solitarywalker@users.noreply.github.com>
2023-04-24 22:37:48 +0900

    JudgeProperty.java修正

--- src/bms/player/beatoraja/play/JudgeProperty.java
+++ src/bms/player/beatoraja/play/JudgeProperty.java
@@ -7,43 +7,48 @@ package bms.player.beatoraja.play;
  */
 public enum JudgeProperty {
 
-    FIVEKEYS(new long[][]{ {-20000, 20000}, {-50000, 50000}, {-100000, 100000}, {-150000, 150000}, {-150000, 500000} },
-            new long[][]{ {-30000, 30000}, {-60000, 60000}, {-110000, 110000}, {-160000, 160000}, {-160000, 500000}},
-            new long[][]{ {-120000, 120000}, {-150000, 150000}, {-200000, 200000}, {-250000, 250000}},
-            new long[][]{ {-130000, 130000}, {-160000, 160000}, {-110000, 110000}, {-260000, 260000}},
-            new boolean[]{true, true, true, false, false, false },
+    FIVEKEYS(
+            new long[][] { { -20000, 20000 }, { -50000, 50000 }, { -100000, 100000 }, { -150000, 150000 },
+                    { -150000, 500000 } },
+            new long[][] { { -30000, 30000 }, { -60000, 60000 }, { -110000, 110000 }, { -160000, 160000 },
+                    { -160000, 500000 } },
+            new long[][] { { -120000, 120000 }, { -150000, 150000 }, { -200000, 200000 }, { -250000, 250000 } },
+            new long[][] { { -130000, 130000 }, { -160000, 160000 }, { -110000, 110000 }, { -260000, 260000 } },
+            new boolean[] { true, true, true, false, false, false },
             MissCondition.ALWAYS,
-            new boolean[]{true, true, true, true, true, false },
-            JudgeWindowRule.NORMAL
-            ),
-    SEVENKEYS(new long[][]{ {-20000, 20000}, {-60000, 60000}, {-150000, 150000}, {-280000, 220000}, {-150000, 500000} },
-            new long[][]{ {-30000, 30000}, {-70000, 70000}, {-160000, 160000}, {-290000, 230000}, {-160000, 500000}},
-            new long[][]{ {-120000, 120000}, {-160000, 160000}, {-200000, 200000}, {-280000, 220000}},
-            new long[][]{ {-130000, 130000}, {-170000, 170000}, {-210000, 210000}, {-290000, 230000}},
-            new boolean[]{true, true, true, false, false, true },
+            new boolean[] { true, true, true, true, true, false },
+            JudgeWindowRule.NORMAL),
+    SEVENKEYS(
+            new long[][] { { -20000, 20000 }, { -60000, 60000 }, { -150000, 150000 }, { -280000, 220000 },
+                    { -150000, 500000 } },
+            new long[][] { { -30000, 30000 }, { -70000, 70000 }, { -160000, 160000 }, { -290000, 230000 },
+                    { -160000, 500000 } },
+            new long[][] { { -120000, 120000 }, { -160000, 160000 }, { -200000, 200000 }, { -280000, 220000 } },
+            new long[][] { { -130000, 130000 }, { -170000, 170000 }, { -210000, 210000 }, { -290000, 230000 } },
+            new boolean[] { true, true, true, false, false, true },
             MissCondition.ALWAYS,
-            new boolean[]{true, true, true, true, true, false },
-            JudgeWindowRule.NORMAL
-            ),
-    PMS(new long[][]{ {-20000, 20000}, {-50000, 50000}, {-117000, 117000}, {-183000, 183000}, {-175000, 500000} },
-            new long[][]{},
-            new long[][]{ {-120000, 120000}, {-150000, 150000}, {-217000, 217000}, {-283000, 283000}},
-            new long[][]{},
-            new boolean[]{true, true, true, false, false, false },
+            new boolean[] { true, true, true, true, true, false },
+            JudgeWindowRule.NORMAL),
+    PMS(new long[][] { { -20000, 20000 }, { -50000, 50000 }, { -117000, 117000 }, { -183000, 183000 },
+            { -175000, 500000 } },
+            new long[][] {},
+            new long[][] { { -120000, 120000 }, { -150000, 150000 }, { -217000, 217000 }, { -283000, 283000 } },
+            new long[][] {},
+            new boolean[] { true, true, true, false, false, false },
             MissCondition.ONE,
-            new boolean[]{true, true, true, false, true, false },
-            JudgeWindowRule.PMS
-            ),
-	KEYBOARD(new long[][]{ {-30000, 30000}, {-90000, 90000}, {-200000, 200000}, {-320000, 240000}, {-200000, 650000} },
-			new long[][]{},
-			new long[][]{ {-160000, 25000}, {-200000, 75000}, {-260000, 140000}, {-320000, 240000}},
-			new long[][]{},
-            new boolean[]{true, true, true, false, false, true },
+            new boolean[] { true, true, true, false, true, false },
+            JudgeWindowRule.PMS),
+    KEYBOARD(
+            new long[][] { { -30000, 30000 }, { -90000, 90000 }, { -200000, 200000 }, { -320000, 240000 },
+                    { -200000, 650000 } },
+            new long[][] {},
+            new long[][] { { -160000, 25000 }, { -200000, 75000 }, { -260000, 140000 }, { -320000, 240000 } },
+            new long[][] {},
+            new boolean[] { true, true, true, false, false, true },
             MissCondition.ALWAYS,
-            new boolean[]{true, true, true, true, true, false },
-            JudgeWindowRule.NORMAL
-			),
-	;
+            new boolean[] { true, true, true, true, true, false },
+            JudgeWindowRule.NORMAL),
+            ;
 
     /**
      * 通常ノートの格判定幅。PG, GR, GD, BD, MSの順で{LATE下限, EARLY上限}のセットで表現する。
@@ -73,10 +78,11 @@ public enum JudgeProperty {
      * 各判定毎のノートの判定を消失するかどうか。PG, GR, GD, BD, PR, MSの順
      */
     public final boolean[] judgeVanish;
-    
+
     public final JudgeWindowRule windowrule;
 
-    private JudgeProperty(long[][] note, long[][] scratch, long[][] longnote, long[][] longscratch, boolean[] combo, MissCondition miss, boolean[] judgeVanish, JudgeWindowRule windowrule) {
+    private JudgeProperty(long[][] note, long[][] scratch, long[][] longnote, long[][] longscratch, boolean[] combo,
+            MissCondition miss, boolean[] judgeVanish, JudgeWindowRule windowrule) {
         this.note = note;
         this.scratch = scratch;
         this.longnote = longnote;
@@ -87,132 +93,133 @@ public enum JudgeProperty {
         this.windowrule = windowrule;
     }
 
-    public int[][] getNoteJudge(int judgerank, int[] judgeWindowRate) {
-    	return convertMilli(windowrule.create(note, judgerank, judgeWindowRate));
+    public int[][] getNoteJudge(int judgerank, int[][] judgeWindowRate) {
+        return convertMilli(windowrule.create(note, judgerank, judgeWindowRate));
     }
 
-    public int[][] getLongNoteEndJudge(int judgerank, int[] judgeWindowRate) {
-    	return convertMilli(windowrule.create(longnote, judgerank, judgeWindowRate));
+    public int[][] getLongNoteEndJudge(int judgerank, int[][] judgeWindowRate) {
+        return convertMilli(windowrule.create(longnote, judgerank, judgeWindowRate));
     }
 
-    public int[][] getScratchJudge(int judgerank, int[] judgeWindowRate) {
-    	return convertMilli(windowrule.create(scratch, judgerank, judgeWindowRate));
+    public int[][] getScratchJudge(int judgerank, int[][] judgeWindowRate) {
+        return convertMilli(windowrule.create(scratch, judgerank, judgeWindowRate));
     }
 
-    public int[][] getLongScratchEndJudge(int judgerank, int[] judgeWindowRate) {
-    	return convertMilli(windowrule.create(longscratch, judgerank, judgeWindowRate));
+    public int[][] getLongScratchEndJudge(int judgerank, int[][] judgeWindowRate) {
+        return convertMilli(windowrule.create(longscratch, judgerank, judgeWindowRate));
     }
-    
+
     private int[][] convertMilli(long[][] judge) {
-    	int[][] mjudge = new int[judge.length][];
-    	for(int i = 0;i < mjudge.length;i++) {
-    		mjudge[i] = new int[judge[i].length];
-    		for(int j = 0;j < mjudge[i].length;j++) {
-        		mjudge[i][j] = (int) (judge[i][j] / 1000);    			
-    		}
-    	}
-    	return mjudge;
+        int[][] mjudge = new int[judge.length][];
+        for (int i = 0; i < mjudge.length; i++) {
+            mjudge[i] = new int[judge[i].length];
+            for (int j = 0; j < mjudge[i].length; j++) {
+                mjudge[i][j] = (int) (judge[i][j] / 1000);
+            }
+        }
+        return mjudge;
     }
-    
-    public long[][] getJudge(NoteType notetype, int judgerank, int[] judgeWindowRate) {
-    	switch(notetype) {
-    	case NOTE:
-        	return windowrule.create(note, judgerank, judgeWindowRate);
-    	case LONGNOTE_END:
-        	return windowrule.create(longnote, judgerank, judgeWindowRate);
-    	case SCRATCH:
-        	return windowrule.create(scratch, judgerank, judgeWindowRate);
-    	case LONGSCRATCH_END:
-        	return windowrule.create(longscratch, judgerank, judgeWindowRate);
-    	default:
-        	return windowrule.create(note, judgerank, judgeWindowRate);
-    	}
+
+    public long[][] getJudge(NoteType notetype, int judgerank, int[][] judgeWindowRate) {
+        switch (notetype) {
+            case NOTE:
+                return windowrule.create(note, judgerank, judgeWindowRate);
+            case LONGNOTE_END:
+                return windowrule.create(longnote, judgerank, judgeWindowRate);
+            case SCRATCH:
+                return windowrule.create(scratch, judgerank, judgeWindowRate);
+            case LONGSCRATCH_END:
+                return windowrule.create(longscratch, judgerank, judgeWindowRate);
+            default:
+                return windowrule.create(note, judgerank, judgeWindowRate);
+        }
     }
-    
+
     public enum MissCondition {
-    	ONE, ALWAYS
+        ONE, ALWAYS
     }
-    
+
     public enum NoteType {
-    	NOTE, LONGNOTE_END, SCRATCH, LONGSCRATCH_END
+        NOTE, LONGNOTE_END, SCRATCH, LONGSCRATCH_END
     }
-    
+
     public enum JudgeWindowRule {
-    	NORMAL (new int[]{25, 50, 75, 100, 125}){
+        NORMAL(new int[] { 25, 50, 75, 100, 125 }) {
 
-			@Override
-			public long[][] create(long[][] org, int judgerank, int[] judgeWindowRate) {
-				return JudgeWindowRule.create(org, judgerank,judgeWindowRate, false);
-			}
-    		
-    	},
-    	PMS (new int[]{33, 50, 70, 100, 133}) {
+            @Override
+            public long[][] create(long[][] org, int judgerank, int[][] judgeWindowRate) {
+                return JudgeWindowRule.create(org, judgerank, judgeWindowRate, false);
+            }
 
-			@Override
-			public long[][] create(long[][] org, int judgerank, int[] judgeWindowRate) {
-				return JudgeWindowRule.create(org, judgerank,judgeWindowRate, true);
-			}
-    		
-    	};
-    	
-    	/**
-    	 * JUDGERANKの倍率(VERYHARD, HARD, NORMAL, EASY, VERYEASY)
-    	 */
-    	public final int[] judgerank;
-    	
-        private static long[][] create(long[][] org, int judgerank, int[] judgeWindowRate, boolean pms) {
-    		final long[][] judge = new long[org.length][2];
-    		final boolean[] fix = pms ? new boolean[]{true, false, false, true, true} : new boolean[]{false, false, false, false, true};
-    		for (int i = 0; i < judge.length; i++) {
-    			for(int j = 0;j < 2;j++) {
-					judge[i][j] = fix[i] ? org[i][j] : org[i][j] * judgerank / 100;
-    			}
-    		}
+        },
+        PMS(new int[] { 33, 50, 70, 100, 133 }) {
 
-    		int fixmin = -1;
-    		for (int i = 0; i < Math.min(org.length, 4); i++) {
-    			if(fix[i]) {
-    				fixmin = i;
-    				continue;
-    			}
-        		int fixmax = -1;
-    			for(int j = i + 1;j < 4;j++) {
-        			if(fix[j]) {
-        				fixmax = j;
-        				break;
-        			}
-    			}
-        		
-    			for(int j = 0;j < 2;j++) {
-					if(fixmin != -1 && Math.abs(judge[i][j]) < Math.abs(judge[fixmin][j])) {
-						judge[i][j] = judge[fixmin][j];
-					}
-					if(fixmax != -1 && Math.abs(judge[i][j]) > Math.abs(judge[fixmax][j])) {
-						judge[i][j] = judge[fixmax][j];
-					}
-    			}
-    		}
+            @Override
+            public long[][] create(long[][] org, int judgerank, int[][] judgeWindowRate) {
+                return JudgeWindowRule.create(org, judgerank, judgeWindowRate, true);
+            }
 
-    		// judgeWindowRateによる補正
-    		for (int i = 0; i < Math.min(org.length, 3); i++) {
-    			for(int j = 0;j < 2;j++) {
-					judge[i][j] = judge[i][j]*judgeWindowRate[i] / 100;
-					if(Math.abs(judge[i][j]) > Math.abs(judge[3][j])) {
-						judge[i][j] = judge[3][j];
-					}
-					if(i > 0 && Math.abs(judge[i][j]) < Math.abs(judge[i - 1][j])) {
-						judge[i][j] = judge[i - 1][j];
-					}
-    			}
-    		}
-    		
-    		return judge;
+        };
+
+        /**
+         * JUDGERANKの倍率(VERYHARD, HARD, NORMAL, EASY, VERYEASY)
+         */
+        public final int[] judgerank;
+
+        private static long[][] create(long[][] org, int judgerank, int[][] judgeWindowRate, boolean pms) {
+            final long[][] judge = new long[org.length][2];
+            final boolean[] fix = pms ? new boolean[] { true, false, false, true, true }
+                    : new boolean[] { false, false, false, false, true };
+            for (int i = 0; i < judge.length; i++) {
+                for (int j = 0; j < 2; j++) {
+                    judge[i][j] = fix[i] ? org[i][j] : org[i][j] * judgerank / 100;
+                }
+            }
+
+            int fixmin = -1;
+            for (int i = 0; i < Math.min(org.length, 4); i++) {
+                if (fix[i]) {
+                    fixmin = i;
+                    continue;
+                }
+                int fixmax = -1;
+                for (int j = i + 1; j < 4; j++) {
+                    if (fix[j]) {
+                        fixmax = j;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < 2; j++) {
+                    if (fixmin != -1 && Math.abs(judge[i][j]) < Math.abs(judge[fixmin][j])) {
+                        judge[i][j] = judge[fixmin][j];
+                    }
+                    if (fixmax != -1 && Math.abs(judge[i][j]) > Math.abs(judge[fixmax][j])) {
+                        judge[i][j] = judge[fixmax][j];
+                    }
+                }
+            }
+
+            // judgeWindowRateによる補正
+            for (int i = 0; i < Math.min(org.length, 3); i++) {
+                for (int j = 0; j < 2; j++) {
+                    judge[i][j] = judge[i][j] * judgeWindowRate[judgerank][i] / 100;
+                    if (Math.abs(judge[i][j]) > Math.abs(judge[3][j])) {
+                        judge[i][j] = judge[3][j];
+                    }
+                    if (i > 0 && Math.abs(judge[i][j]) < Math.abs(judge[i - 1][j])) {
+                        judge[i][j] = judge[i - 1][j];
+                    }
+                }
+            }
+
+            return judge;
         }
-        
+
         private JudgeWindowRule(int[] judgerank) {
-        	this.judgerank = judgerank;
+            this.judgerank = judgerank;
         }
-        
-    	public abstract long[][] create(long[][] org, int judgerank, int[] judgeWindowRate);
+
+        public abstract long[][] create(long[][] org, int judgerank, int[][] judgeWindowRate);
     }
 }
