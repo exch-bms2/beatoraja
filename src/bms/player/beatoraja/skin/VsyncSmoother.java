2f439f50f79955189d519782b3dd95745eaab4da
박현우 <phu54321@naver.com>
2025-11-27 00:21:06 +0900

    feat: add proper license header

--- src/bms/player/beatoraja/skin/VsyncSmoother.java
+++ src/bms/player/beatoraja/skin/VsyncSmoother.java
@@ -1,3 +1,20 @@
+// Copyright (C) 2025 Park Hyunwoo
+//
+// This file is part of beatoraja.
+//
+// beatoraja is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// beatoraja is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with beatoraja.  If not, see <https://www.gnu.org/licenses/>.
+
 package bms.player.beatoraja.skin;
 
 import com.badlogic.gdx.Gdx;
