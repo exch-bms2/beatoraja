187b78d5073260fd0d9eba6d7900740133c09038
exch-bms2 <exch-bms2@users.noreply.github.com>
2026-06-22 08:10:33 +0900

    JsonSkinConfigurationSkinObjectLoader : add parser skin-preview

--- src/bms/player/beatoraja/skin/json/JsonSkinConfigurationSkinObjectLoader.java
+++ src/bms/player/beatoraja/skin/json/JsonSkinConfigurationSkinObjectLoader.java
@@ -1,7 +1,11 @@
 package bms.player.beatoraja.skin.json;
 
 import bms.player.beatoraja.config.SkinConfigurationSkin;
+import bms.player.beatoraja.config.SkinPreview;
 import bms.player.beatoraja.skin.SkinHeader;
+import bms.player.beatoraja.skin.SkinObject;
+
+import java.nio.file.Path;
 
 public class JsonSkinConfigurationSkinObjectLoader extends JsonSkinObjectLoader<SkinConfigurationSkin> {
 
@@ -12,6 +16,15 @@ public class JsonSkinConfigurationSkinObjectLoader extends JsonSkinObjectLoader<
 	@Override
 	public SkinConfigurationSkin getSkin(SkinHeader header) {
 		return new SkinConfigurationSkin(header);
+	}
+
+	@Override
+	public SkinObject loadSkinObject(SkinConfigurationSkin skin, JsonSkin.Skin sk, JsonSkin.Destination dst, Path p) {
+		SkinObject obj = super.loadSkinObject(skin, sk, dst, p);
+		if (obj != null) {
+			return obj;
+		}
+		return dst.id.equals("skin-preview") ? new SkinPreview() : null;
 	}
 
 }
